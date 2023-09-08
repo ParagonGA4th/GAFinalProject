3daa586b2fa9caafa3dd75f00b2641c54d4b0175
Jisang Byeon <pannasonic74@gmail.com>
2023-09-08 19:59:34 +0900

    [변지상] 프로젝트별 더미 생성

--- ParagonEngine/ParagonGameEngine/DLLTest.cpp
+++
@@ -1,6 +0,0 @@
-#include "DLLTest.h"
-
-double Sum(double a, double b)
-{
-	return a + b;
-}
---
+++ ParagonEngine/ParagonGameEngine/EngineMain.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/// <summary>
+/// 2023.09.08
+/// </summary>
+
+namespace Pg::Engine
+{
+	class EngineMain
+	{
+	public:
+		void Initialize();
+		void Finalize();
+	};
+}
+
