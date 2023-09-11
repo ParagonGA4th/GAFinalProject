211ec7b9078a38ce6d35e0977595ba8847aa2c18
Jisang Byeon <pannasonic74@gmail.com>
2023-09-11 10:42:09 +0900

    [변지상] 윈도우 초기화 분리 성공

--- ParagonEngine/ParagonClient/Entry.cpp
+++ ParagonEngine/ParagonClient/Entry.cpp
@@ -16,91 +16,19 @@
 
 ///Paragon Engine의 Entry Point. 
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
-// 프로그램의 최초 진입점(Entry Point). 
-int WINAPI WinMain(_In_ HINSTANCE hInstance,
+int APIENTRY WinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPSTR	  lpCmdLine,
 	_In_ int		  nCmdShow)
 {
-	char	  szAppName[] = "Paragon Engine";
-	HWND	  hWnd;
-	MSG      msg;
-	WNDCLASS wndclass;
-
-	LPCWSTR appName = L"Paragon Engine";
-
-	wndclass.style = CS_HREDRAW | CS_VREDRAW;
-	wndclass.lpfnWndProc = WndProc;
-	wndclass.cbClsExtra = 0;
-	wndclass.cbWndExtra = 0;
-	wndclass.hInstance = hInstance;
-	wndclass.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
-	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wndclass.lpszMenuName = NULL;
-	wndclass.lpszClassName = L"Paragon Engine";
-
-	/// 윈도 클래스 등록
-	RegisterClass(&wndclass);
-
-	/// 윈도 생성
-	hWnd = CreateWindow(wndclass.lpszClassName,
-		appName,
-		WS_OVERLAPPEDWINDOW,
-		100, 100, 800, 600,
-		NULL, NULL, hInstance, NULL);
-
-	if (!hWnd) return FALSE;
 
 	//테스트용 초기화(유니크 포인터)
 	std::unique_ptr<Pg::Core::CoreMain> main;
 	main = std::make_unique<Pg::Core::CoreMain>();
-	main->Initialize();
+	main->Initialize(static_cast<void*>(hInstance), nCmdShow);
 
-	/// 생성된 윈도를 화면에 표시
-	ShowWindow(hWnd, nCmdShow);
-	UpdateWindow(hWnd);
-	
+	main->Update();
 
-	/// 메시지 루프
-	while (TRUE)
-	{
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-		{
-			if (msg.message == WM_QUIT) break;
-			DispatchMessage(&msg);
-		}
-		else
-		{
-			Sleep(10);
-		}
-	}
-
-	return (int)msg.wParam;
-}
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
-	WPARAM wParam, LPARAM lParam)
-{
-	HDC			hdc;
-	PAINTSTRUCT ps;
-
-	switch (message)
-	{
-		case WM_PAINT:
-			hdc = BeginPaint(hWnd, &ps);
-			EndPaint(hWnd, &ps);
-			break;
-
-		case WM_DESTROY:
-			PostQuitMessage(0);
-			break;
-
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return 0;
+	main->Finalize();
 }
 
