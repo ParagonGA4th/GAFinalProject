bcfaa3b8297f110a3a13e56c2e8a1f4b03f54d32
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2023-09-12 14:27:17 +0900

    [고태욱] 싱글턴 클래스 자식이 사용할 수 있게 수정 + 강화

--- ParagonEngine/ParagonCore/Singleton.h
+++ ParagonEngine/ParagonCore/Singleton.h
@@ -6,11 +6,21 @@ template<typename T>
 class Singleton
 {
 public:
-	static T* instance();
+	static T* Instance();
+
+protected:
+	Singleton() = default;
+
+private:
+	Singleton(const Singleton& rhs) = delete;
+	Singleton(Singleton&& rhs) = delete;
+	Singleton& operator=(const Singleton& rhs) = delete;
+	Singleton& operator=(Singleton&& rhs) = delete;
+
 };
 
 template<typename T>
-inline T* Singleton<T>::instance()
+inline T* Singleton<T>::Instance()
 {
 	static T* instance = new T();
 	return instance;
