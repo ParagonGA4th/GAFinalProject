826e30cf196fd91b4af4567c381de24158ad9c28
Jisang Byeon <pannasonic74@gmail.com>
2023-09-12 13:00:10 +0900

    [변지상] 싱글턴 작업 및 시스템 작업 중

--- ParagonEngine/ParagonGameEngine/TimeSystem.h
+++ ParagonEngine/ParagonGameEngine/TimeSystem.h
@@ -1,5 +1,35 @@
 #pragma once
-class TimeSystem
+
+#include "../ParagonCore/Singleton.h"
+#include <windows.h>
+
+/// <summary>
+/// ParagonEngine¿« TimeSystem.
+/// 2023.09.12
+/// </summary>
+namespace Pg::Engine::Time
 {
-};
+	class TimeSystem : public Singleton<TimeSystem>
+	{
+	public:
+		TimeSystem();
+
+		void Initialize();
+		void TimeMeasure();
+		void MeasureFrame(float deltaTime);
+
+		static float GetDeltaTime();
+		static int GetFrameRate();
+
+	public:
+		static float _deltaTime;
+		static int _frameRate;
+		float _totalTick;
+
+		LARGE_INTEGER _frequency;
+		LARGE_INTEGER _startTick;
+		LARGE_INTEGER _endTick;
+	};
+}
+
 
