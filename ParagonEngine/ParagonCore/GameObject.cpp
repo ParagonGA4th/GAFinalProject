b626479e9e50472678bf02e37045fb5385a6d3c6
Jisang Byeon <pannasonic74@gmail.com>
2023-09-12 11:20:34 +0900

    [변지상] 엔진 작업중

--- ParagonEngine/ParagonCore/GameObject.cpp
+++ ParagonEngine/ParagonCore/GameObject.cpp
@@ -1,1 +1,39 @@
 #include "GameObject.h"
+
+namespace Pg::Core
+{
+	GameObject::GameObject(const std::string& name)
+	{
+
+	}
+
+	GameObject::~GameObject()
+	{
+
+	}
+
+	void GameObject::Awake()
+	{
+
+	}
+
+	void GameObject::Start()
+	{
+
+	}
+
+	void GameObject::Update()
+	{
+
+	}
+
+	void GameObject::FixedUpdate()
+	{
+
+	}
+
+	void GameObject::LateUpdate()
+	{
+
+	}
+}
