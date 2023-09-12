e7e19339e2fc7de6cd35a21b67743471ad1951b1
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2023-09-12 17:42:00 +0900

    [고태욱] Resource 관리 시스템 레이아웃 세팅 (AssetManager & Resource 기본 형태)

--- ParagonEngine/ParagonCore/AssetManager.h
+++ ParagonEngine/ParagonCore/AssetManager.h
@@ -1,17 +1,25 @@
 #pragma once
+#include "Singleton.h"
 
+/// <summary>
+/// 게임 엔진 내의 모든 애셋/리소스 내용을 총괄할 매니저.
+/// </summary>
 
-namespace Pg::Core::Managers
+namespace Pg::Core::Manager
 {
-	class AssetManager
+	class AssetManager : public Singleton<AssetManager>
 	{
 	public:
-		
+		AssetManager();
+		~AssetManager();
 
+
+
+	private:
+		
+		
 
 
 
 	};
 }
-
-
