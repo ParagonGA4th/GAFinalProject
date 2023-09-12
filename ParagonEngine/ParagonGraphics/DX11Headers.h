1a9f8c6ed733f5cec1abc909549519d82a5e3ff3
Jeongsik Kim <thehc9@gmail.com>
2023-09-12 20:33:36 +0900

    [김정식] 그래픽스 엔진 틀 작성
    - LowDX11Logic 클래스와 LowDX11Storage 클래스 추가 후, 틀 작성.
    - ParagonRenderer 클래스 추가

---
+++ ParagonEngine/ParagonGraphics/DX11Headers.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <dxgi1_2.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+#include <windows.h>
+
+#pragma comment(lib, "Dxgi.lib")
+#pragma comment(lib, "D3D11.lib")
+#pragma comment(lib, "D3DCompiler.lib")
+
+using float2 = DirectX::XMFLOAT2;
+using float3 = DirectX::XMFLOAT3;
+using float4 = DirectX::XMFLOAT4;
+using matrix = DirectX::XMMATRIX;
--- ParagonEngine/ParagonGraphics/GraphicsMain.h
+++ ParagonEngine/ParagonGraphics/GraphicsMain.h
@@ -2,14 +2,28 @@
 #include "../ParagonGraphics/GraphicsDLLExporter.h"
 #include "../ParagonCore/IEngine.h"
 
+#include <windows.h>
+//#include "DX11Headers.h"
+
+
 namespace Pg::Graphics
 {
+	class LowDX11Logic;
+	class LowDX11Storage;
+
 	class GraphicsMain : public Pg::Core::IEngine
 	{
+	public:
+		PARAGON_GRAPHICS_DLL GraphicsMain();
+
 	public:
 		PARAGON_GRAPHICS_DLL void Initialize();
 		PARAGON_GRAPHICS_DLL void Update();
 		PARAGON_GRAPHICS_DLL void Finalize();
+
+	private:
+		LowDX11Logic* _DXLogic;
+		LowDX11Storage* _DXStorage;
 	};
 }
 
