c84c158da8c6a94b510b69739887de34ecbdca9a
Jisang Byeon <pannasonic74@gmail.com>
2023-09-12 10:12:55 +0900

    [변지상] 컴포넌트 작업 추가 및 다른 클래스 생성

--- ParagonEngine/ParagonGraphics/GraphicsMain.h
+++ ParagonEngine/ParagonGraphics/GraphicsMain.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "../ParagonGraphics/GraphicsDLLExporter.h"
+#include "../ParagonCore/IEngine.h"
 
 namespace Pg::Graphics
 {
-	class GraphicsMain
+	class GraphicsMain : public Pg::Core::IEngine
 	{
 	public:
 		PARAGON_GRAPHICS_DLL void Initialize();
+		PARAGON_GRAPHICS_DLL void Update();
 		PARAGON_GRAPHICS_DLL void Finalize();
 	};
 }
