0bb4236f736fba16bb36f03b76d770158e3316de
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2023-09-13 10:32:57 +0900

    [고태욱] 리소스 매니저 기본 형태 추가, 작업 중. => String만 core에서 관리하고, 이에 대한 구조체를 별도로 그래픽 엔진의 리소스 매니저와 연동하는 방향으로 바꾸는 중.

--- ParagonEngine/ParagonCore/AssetManager.cpp
+++ ParagonEngine/ParagonCore/AssetManager.cpp
@@ -1,1 +1,30 @@
 #include "AssetManager.h"
+
+namespace Pg::Core::Manager
+{
+	AssetManager::AssetManager()
+	{
+
+	}
+
+	AssetManager::~AssetManager()
+	{
+
+	}
+
+	void AssetManager::Unload(const std::string& path)
+	{
+		//리소스 활용을 위해 weak_ptr.lock()으로 체크.
+		auto res = resources[path].lock();
+
+		//std::shared_ptr이 null이 아니라면, 발동.
+		if (res)
+		{
+			res->InternalUnload();
+			resources.erase(path);
+		}
+	}
+
+
+
+}
--- ParagonEngine/ParagonCore/BaseResource.h
+++ ParagonEngine/ParagonCore/BaseResource.h
@@ -1,10 +1,26 @@
 #pragma once
+#include "ResourceType.h"
+#include <string>
 
-namespace Pg::Core
+/// <summary>
+/// 리소스 매니저에 기록될 모든 리소스의 기본.
+/// </summary>
+
+namespace Pg::Core::Resources
 {
 	class BaseResource
 	{
+	public:
+		BaseResource(const std::string& filePath);
+		virtual ~BaseResource();
 
+		//내부적으로 로드, 언로드해주는 함수들. 최종 자식 클래스들의 구현체의 생성/소멸자에서 호출되어야.
+		virtual void InternalLoad() abstract;
+		virtual void InternalUnload() abstract;
+
+	protected:
+		std::string _filePath;
+		Pg::Core::Enum::eResourceType _resourceType;
 	};
 }
 
