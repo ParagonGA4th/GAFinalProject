869a2512d4b195fad74133ce050fbcdfb05d0613
Jisang Byeon <pannasonic74@gmail.com>
2023-09-13 12:03:06 +0900

    [변지상] 엔진 데이터 복구

--- ParagonEngine/ParagonGameEngine/EngineMain.h
+++ ParagonEngine/ParagonGameEngine/EngineMain.h
@@ -1,6 +1,11 @@
 #pragma once
+
 #include "../ParagonGameEngine/EngineDLLExporter.h"
 #include "../ParagonCore/IEngine.h"
+#include "../ParagonCore/Singleton.h"
+#include "../ParagonUtil/Log.h"
+#include "InputSystem.h"
+#include "TimeSystem.h"
 
 /// <summary>
 /// 2023.09.08
@@ -11,9 +16,16 @@ namespace Pg::Engine
 	class EngineMain : public Pg::Core::IEngine
 	{
 	public:
-		PARAGON_ENGINE_DLL void Initialize();
-		PARAGON_ENGINE_DLL void Update();
-		PARAGON_ENGINE_DLL void Finalize();
+		PARAGON_ENGINE_DLL EngineMain();
+
+		PARAGON_ENGINE_DLL void Initialize(float width, float height) override;
+		PARAGON_ENGINE_DLL void Update() override;
+		PARAGON_ENGINE_DLL void Finalize() override;
+
+	private:
+
+		Time::TimeSystem* _timeSystem;
+		Input::InputSystem* _inputSystem;
 	};
 }
 
