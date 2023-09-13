8015463251b5c1d1184fe2cf15d5a95104a91ea6
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2023-09-13 10:32:57 +0900

    [고태욱] 리소스 매니저 기본 형태 추가, 작업 중. => String만 core에서 관리하고, 이에 대한 구조체를 별도로 그래픽 엔진의 리소스 매니저와 연동하는 방향으로 바꾸는 중.

--- ParagonEngine/ParagonCore/BaseResource.cpp
+++ ParagonEngine/ParagonCore/BaseResource.cpp
@@ -1,6 +1,24 @@
 #include "BaseResource.h"
+#include "ResourceHelper.h"
+#include <filesystem>
 
-namespace Pg::Core
+namespace Pg::Core::Resources
 {
+	using Pg::Core::Enum::eResourceType;
+	using Pg::Core::Helper::ResourceHelper;
+
+	BaseResource::BaseResource(const std::string& filePath) : _filePath(filePath)
+	{
+		std::filesystem::path tPath(filePath);
+		std::string tExt = tPath.extension().string();
+		this->_resourceType = ResourceHelper::Ext2ResourceType(tExt);
+
+		//자식은 이제 실제로 _resourceType을 활용하던, 자기 자신 내부의 정부를 Load할 수 있어야 한다.
+	}
+
+	BaseResource::~BaseResource()
+	{
+
+	}
 
 }
