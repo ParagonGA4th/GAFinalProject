3e590aca905da689b238d5e80d1e2a8b18386d43
Jisang Byeon <pannasonic74@gmail.com>
2023-09-13 12:26:01 +0900

    [변지상] 오류 1차 수정

--- ParagonEngine/ParagonCore/IGraphics.h
+++ ParagonEngine/ParagonCore/IGraphics.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <windows.h>
+
 /// <summary>
 /// 변지상의 엔진 인터페이스. 코어에 들어갈 예정
 /// 2023.09.11
@@ -9,9 +11,12 @@ namespace Pg::Core
 	class IGraphics abstract
 	{
 	public:
-		virtual void  Initialize() abstract;
+		virtual void  Initialize(HWND hWnd, int screenWidth, int screenHeight) abstract;
 
 		virtual void Update() abstract;
+		virtual void BeginRender() abstract;
+		virtual void Render() abstract;
+		virtual void EndRender() abstract;
 
 		virtual void Finalize() abstract;
 	};
