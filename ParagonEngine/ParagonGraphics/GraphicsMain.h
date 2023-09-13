d5093978e1b8b854f58ad714b8d4e86f607a2cd2
Jeongsik Kim <thehc9@gmail.com>
2023-09-13 10:49:14 +0900

    [김정식] Graphics Main에 BeginRendr, Render, Endrender 추가

--- ParagonEngine/ParagonGraphics/GraphicsMain.h
+++ ParagonEngine/ParagonGraphics/GraphicsMain.h
@@ -5,24 +5,30 @@
 #include <windows.h>
 //#include "DX11Headers.h"
 
-
 namespace Pg::Graphics
 {
 	class LowDX11Logic;
 	class LowDX11Storage;
+
 	class GraphicsMain : public Pg::Core::IGraphics
 	{
 	public:
 		PARAGON_GRAPHICS_DLL GraphicsMain();
 
 	public:
-		PARAGON_GRAPHICS_DLL void Initialize();
+		PARAGON_GRAPHICS_DLL void Initialize(HWND hWnd);
 		PARAGON_GRAPHICS_DLL void Update();
+		PARAGON_GRAPHICS_DLL void BeginRender();
+		PARAGON_GRAPHICS_DLL void Render();
+		PARAGON_GRAPHICS_DLL void EndRender();
 		PARAGON_GRAPHICS_DLL void Finalize();
 
 	private:
 		LowDX11Logic* _DXLogic;
 		LowDX11Storage* _DXStorage;
+
+	private:
+		HRESULT hr;
 	};
 }
 
