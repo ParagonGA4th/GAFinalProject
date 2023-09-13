69748cb55b56895cf4e3cb324732fe7833b7f748
Minseo Choi <78577815+MinseoChoi03@users.noreply.github.com>
2023-09-13 11:16:03 +0900

    core의 window 관련 함수들 제거, editor로 옮김

--- ParagonEngine/ParagonCore/CoreMain.cpp
+++ ParagonEngine/ParagonCore/CoreMain.cpp
@@ -7,40 +7,20 @@
 
 namespace Pg::Core
 {
-	CoreMain::CoreMain() :
-		_hWnd(), _msg(),
-		_screenwidth(1920),
-		_screenheight(1080),
-		_className(L"ParagonEngine"),
-		_windowName(L"ParagonEngine")
+	CoreMain::CoreMain()
 	{
 		_engine = std::make_unique<Pg::Engine::EngineMain>();
 		_graphics = std::make_unique<Pg::Graphics::GraphicsMain>();
 		_logger = std::make_unique<Pg::Util::Debug::Log>();
-
-		_inputSystem = std::make_unique<Pg::Engine::Input::InputSystem>();
 	}
-
 
 	CoreMain::~CoreMain()
 	{
 
 	}
 
-	long CoreMain::Initialize(void* hInstance, int cmdShow)
+	long CoreMain::Initialize(int screenWidth, int screenHeight)
 	{
-
-		//윈도우 초기화
-		RegisterClass((HINSTANCE)hInstance);
-
-		RECT rect;
-		GetClientRect(_hWnd, &rect);
-
-		if (!CreateWindows((HINSTANCE)hInstance, cmdShow))
-		{
-			return S_FALSE;
-		}
-
 		//엔진 초기화
 		_engine->Initialize();
 		_graphics->Initialize();
@@ -48,10 +28,6 @@ namespace Pg::Core
 		//디버그 초기화
 		_logger->Initialize();
 		_logger->SetLoggerLevel(0);
-
-		// InputSystem 초기화
-		_inputSystem->Initialize(_screenwidth, _screenheight);
-
 
 		PG_TRACE("Engine Success!!");
 		PG_DEBUG("Engine Success!!");
@@ -63,99 +39,28 @@ namespace Pg::Core
 
 	void CoreMain::Update()
 	{
-		while (true)
-		{
-			if (PeekMessage(&_msg, NULL, 0, 0, PM_REMOVE))
-			{
-				if (_msg.message == WM_QUIT)
-				{
-					break;
-				}
+		//여기다가 시스템 싹 다 업데이트!!
+		_engine->Update();
+		_graphics->Update();
+	}
 
-				DispatchMessage(&_msg);
-				_inputSystem->HandleMessage(_msg);
-			}
-			else
-			{
-				//여기다가 시스템 싹 다 업데이트!!
-				_engine->Update();
-				_graphics->Update();
-				_inputSystem->Update();
-			}
-		}
+	void CoreMain::BeginRender()
+	{
+
+	}
+
+	void CoreMain::Render()
+	{
+
+	}
+
+	void CoreMain::EndRender()
+	{
+
 	}
 
 	void CoreMain::Finalize()
 	{
 
-	}
-
-	ATOM CoreMain::RegisterClass(HINSTANCE hInstance)
-	{
-		WNDCLASSEXW wcex;
-
-		wcex.cbSize = sizeof(WNDCLASSEX);
-
-		wcex.style = CS_HREDRAW | CS_VREDRAW;
-		wcex.lpfnWndProc = WndProc;
-		wcex.cbClsExtra = 0;
-		wcex.cbWndExtra = 0;
-		wcex.hInstance = hInstance;
-		wcex.hIcon = NULL;
-		wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-		wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-		wcex.lpszMenuName = NULL;
-		wcex.lpszClassName = _className;
-		wcex.hIconSm = NULL;
-
-		return RegisterClassExW(&wcex);
-	}
-
-	BOOL CoreMain::CreateWindows(HINSTANCE hInstance, int cmdShow)
-	{
-		_hWnd = CreateWindowW(_className, _windowName, WS_OVERLAPPEDWINDOW,
-			CW_USEDEFAULT, CW_USEDEFAULT, _screenwidth, _screenheight, nullptr, nullptr, hInstance, nullptr);
-
-		if (!_hWnd)
-		{
-			return FALSE;
-		}
-
-		ShowWindow(_hWnd, cmdShow);
-		UpdateWindow(_hWnd);
-
-		return TRUE;
-	}
-
-	LRESULT CALLBACK CoreMain::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-	{
-		switch (message)
-		{
-		case WM_LBUTTONDOWN:
-			return 0;
-
-		case WM_MBUTTONDOWN:
-			return 0;
-
-		case WM_RBUTTONDOWN:
-			return 0;
-
-		case WM_LBUTTONUP:
-
-		case WM_MBUTTONUP:
-
-		case WM_RBUTTONUP:
-			return 0;
-
-		case WM_MOUSEMOVE:
-			break;
-
-		case WM_DESTROY:
-			PostQuitMessage(0);
-			break;
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
-		}
-		return 0;
 	}
 }
