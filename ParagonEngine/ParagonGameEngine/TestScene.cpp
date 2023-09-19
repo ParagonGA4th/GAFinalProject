42fab5268dd3f3e8cac7e8b1e2f34245691ceecd
Jeongsik Kim <thehc9@gmail.com>
2023-09-19 13:50:53 +0900

    [김정식] 임시 카메라 관련 코드 삭제

--- ParagonEngine/ParagonGameEngine/TestScene.cpp
+++ ParagonEngine/ParagonGameEngine/TestScene.cpp
@@ -12,10 +12,13 @@ void Pg::Engine::TestScene::Initialize()
 	using namespace Pg::Core;
 
 	tCurrentScene = new Scene("TestCurrentScene");
+
 	GameObject* tObj1 = new GameObject("Cube1");
 	tObj1->GetComponent<Transform>()->SetPosition({ 10.0f, 0.0f, 5.0f });
+
 	GameObject* tObj2 = new GameObject("Cube2");
 	tObj2->GetComponent<Transform>()->SetPosition({ -10.0f, 0.0f, 5.0f });
+
 	tCurrentScene->AddObject(tObj1);
 	tCurrentScene->AddObject(tObj2);
 }
--- ParagonEngine/ParagonGraphics/GraphicsMain.h
+++ ParagonEngine/ParagonGraphics/GraphicsMain.h
@@ -62,9 +62,6 @@ namespace Pg::Graphics
 	private:
 		LowDX11Logic* _DXLogic;
 		LowDX11Storage* _DXStorage;
-
-		//TempCamera* _camera;
-
 	};
 }
 
