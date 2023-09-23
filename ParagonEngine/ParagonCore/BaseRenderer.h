7f57ee0b64ccadbcb64645d1818db4cb22c86f50
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2023-09-23 12:08:24 +0900

    [고태욱] BaseRenderer 컴포넌트 기본 수정.

--- ParagonEngine/ParagonCore/BaseRenderer.h
+++ ParagonEngine/ParagonCore/BaseRenderer.h
@@ -1,5 +1,14 @@
 #pragma once
-class BaseRenderer
+
+/// <summary>
+/// 앞으로 렌더할 때 모든 기본 렌더러들이 상속받을 기본 클래스.
+/// </summary>
+
+namespace Pg::Core::Components
 {
-};
+	class BaseRenderer
+	{
+	};
+}
+
 
