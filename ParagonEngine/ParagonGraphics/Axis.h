33e400438db1976507f71dde64dd3313355c2735
Jeongsik Kim <thehc9@gmail.com>
2023-09-26 11:46:37 +0900

    [김정식] Axis, Grid 파일 추가

--- ParagonEngine/ParagonGraphics/Axis.h
+++ ParagonEngine/ParagonGraphics/Axis.h
@@ -1,5 +1,29 @@
 #pragma once
-class Axis
-{
-};
 
+#include "DX11Headers.h"
+
+namespace Pg::Graphics
+{
+	class LowDX11Storage;
+
+	struct Vertex
+	{
+		float3 Pos;
+		float4 Color;
+	};
+
+	class Axis
+	{
+	public:
+		Axis();
+
+	public:
+		void Draw();
+
+	private:
+		LowDX11Storage* _DXStorage;
+
+		ID3D11Buffer* VB;
+		ID3D11Buffer* IB;
+	};
+}
