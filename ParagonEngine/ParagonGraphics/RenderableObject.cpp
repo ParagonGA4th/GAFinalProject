dcb562a33392284adc2aada45f5152e4e5e4b4f1
Jeongsik Kim <thehc9@gmail.com>
2023-09-26 20:40:25 +0900

    [김정식] RenderableObject 상속 구조로 변경

---
+++ ParagonEngine/ParagonGraphics/RenderableObject.cpp
@@ -0,0 +1,89 @@
+#include "RenderableObject.h"
+
+#include "LowDX11Storage.h"
+#include "VertexShader.h"
+#include "PixelShader.h"
+
+Pg::Graphics::RenderableObject::RenderableObject()
+{
+	_DXStorage = Pg::Graphics::LowDX11Storage::GetInstance();
+
+}
+
+Pg::Graphics::RenderableObject::~RenderableObject()
+{
+
+}
+
+void Pg::Graphics::RenderableObject::Initialize()
+{
+	BuildBuffers();
+	BindBuffers();
+}
+
+void Pg::Graphics::RenderableObject::Update(float time)
+{
+	// 상수버퍼 업데이트
+	for (auto& e : _vertexShader->_constantBuffers)
+	{
+		e->Update();
+	}
+}
+
+void Pg::Graphics::RenderableObject::Draw()
+{
+
+}
+
+void Pg::Graphics::RenderableObject::BuildBuffers()
+{
+
+}
+
+void Pg::Graphics::RenderableObject::BindBuffers()
+{
+
+}
+
+void Pg::Graphics::RenderableObject::BindShaders()
+{
+	_vertexShader->Bind();
+	_pixelShader->Bind();
+}
+
+void Pg::Graphics::RenderableObject::UnbindShaders()
+{
+	_vertexShader->UnBind();
+	_pixelShader->UnBind();
+}
+
+void Pg::Graphics::RenderableObject::BindInputLayout()
+{
+	_DXStorage->_deviceContext->IASetInputLayout(_vertexShader->_inputLayout);
+	_DXStorage->_deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+}
+
+void Pg::Graphics::RenderableObject::UnbindInputLayout()
+{
+	_DXStorage->_deviceContext->IASetInputLayout(nullptr);
+}
+
+void Pg::Graphics::RenderableObject::SetVertexShader(VertexShader* shader)
+{
+	_vertexShader = shader;
+}
+
+void Pg::Graphics::RenderableObject::SetPixelShader(PixelShader* shader)
+{
+	_pixelShader = shader;
+}
+
+Pg::Graphics::VertexShader* Pg::Graphics::RenderableObject::GetVertexShader()
+{
+	return _vertexShader;
+}
+
+Pg::Graphics::PixelShader* Pg::Graphics::RenderableObject::GetPixelShader()
+{
+	return _pixelShader;
+}
