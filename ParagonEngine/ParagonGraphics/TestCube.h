49c4c558ffba80d5c3405f8f3a7117acb7d5e2d5
Jeongsik Kim <thehc9@gmail.com>
2023-09-26 11:45:35 +0900

    [김정식] 오브젝트가 Shader를 갖고 있도록 구조 변경

--- ParagonEngine/ParagonGraphics/TestCube.h
+++ ParagonEngine/ParagonGraphics/TestCube.h
@@ -13,6 +13,8 @@ struct BoxCBData
 namespace Pg::Graphics
 {
 	class LowDX11Storage;
+	class VertexShader;
+	class PixelShader;
 
 	class TestCube
 	{
@@ -20,21 +22,41 @@ namespace Pg::Graphics
 		TestCube(LowDX11Storage* storage);
 
 	public:
+		void Initialize();
 		void Update(float time);
 		void Draw();
 
 	private:
+		LowDX11Storage* _DXStorage;
 
-		struct MeshVertex
+		struct TestCubeVertex
 		{
 			float3 position;
 			float3 color;
 		};
 
-		LowDX11Storage* _DXStorage;
-
 		ID3D11Buffer* VB = nullptr;
 		ID3D11Buffer* IB = nullptr;
+
+		VertexShader* _vertexShader;
+		PixelShader* _pixelShader;
+
+	private:
+		void BuildBuffers();
+		void BindBuffers();
+
+		void BindShaders();
+		void UnbindShaders();
+
+		void BindInputLayout();
+		void UnbindInputLayout();
+
+	public:
+		void SetVertexShader(VertexShader* shader);
+		void SetPixelShader(PixelShader* shader);
+
+		VertexShader* GetVertexShader();
+		PixelShader* GetPixelShader();
 
 	public:
 		BoxCBData _cbData;
