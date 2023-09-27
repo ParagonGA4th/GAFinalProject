f504935f39ef58523ef90406a843b88bf0d97e2c
Jisang Byeon <pannasonic74@gmail.com>
2023-09-27 14:01:23 +0900

    [변지상]

--- ParagonEngine/ParagonAPI/PhysXTest.cpp
+++ ParagonEngine/ParagonAPI/PhysXTest.cpp
@@ -21,6 +21,7 @@ void main()
 
 	physx::PxPvd* mPvd = NULL;
 
+	///여기서부터 초기화 시작!
 	// foundation 세팅 및 체크.
 	mFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, mDefaultAllocatorCallback, mDefaultErrorCallback);
 	if (!mFoundation) throw("PxCreateFoundation failed!");
@@ -31,4 +32,15 @@ void main()
 
 	mToleranceScale.length = 100;        // typical length of an object
 	mToleranceScale.speed = 981;         // typical speed of an object, gravity*1s is a reasonable choice
+
+	//본격적으로 PhysX 생성
+	mPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *mFoundation, mToleranceScale, true, mPvd);
+
+	//아마 PhysX의 Scene에 대한 설정 같아보인다.
+	physx::PxSceneDesc sceneDesc(mPhysics->getTolerancesScale());
+	sceneDesc.gravity = physx::PxVec3(0.0f, -9.81f, 0.0f);
+	sceneDesc.cpuDispatcher = mDispatcher;
+	sceneDesc.filterShader = physx::PxDefaultSimulationFilterShader;
+
+	mScene = mPhysics->createScene(sceneDesc);
 }
