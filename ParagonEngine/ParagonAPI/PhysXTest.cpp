259af54b7c33c1935fe73e9616726ed64a5d39cc
Jisang Byeon <pannasonic74@gmail.com>
2023-09-27 10:08:55 +0900

    [변지상] PhysX 계속 초기화중

--- ParagonEngine/ParagonAPI/PhysXTest.cpp
+++ ParagonEngine/ParagonAPI/PhysXTest.cpp
@@ -13,4 +13,22 @@ void main()
 	physx::PxTolerancesScale mToleranceScale;
 
 	physx::PxFoundation* mFoundation = NULL;
+
+	physx::PxPhysics* mPhysics = NULL;
+
+	physx::PxScene* mScene = NULL;
+	physx::PxMaterial* mMaterial = NULL;
+
+	physx::PxPvd* mPvd = NULL;
+
+	// foundation 세팅 및 체크.
+	mFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, mDefaultAllocatorCallback, mDefaultErrorCallback);
+	if (!mFoundation) throw("PxCreateFoundation failed!");
+
+	mPvd = PxCreatePvd(*mFoundation);
+	physx::PxPvdTransport* transport = physx::PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
+	mPvd->connect(*transport, physx::PxPvdInstrumentationFlag::eALL);
+
+	mToleranceScale.length = 100;        // typical length of an object
+	mToleranceScale.speed = 981;         // typical speed of an object, gravity*1s is a reasonable choice
 }
