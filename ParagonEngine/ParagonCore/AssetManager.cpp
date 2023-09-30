33123e085a488b158074e85ea4773a95a4de5393
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2023-09-30 15:34:18 +0900

    [고태욱] 추석 기간 중 Refactoring, 개인 브랜치 확장 (장소이동 용이 + 빠른 워크플로우를 위해 설정.)

--- ParagonEngine/ParagonCore/AssetManager.cpp
+++ ParagonEngine/ParagonCore/AssetManager.cpp
@@ -1,10 +1,5 @@
-// #TW_WIP AssetManager에서 언로드가 오류를 일으켜, 임시로 링크에러를 피하기 위해 주석 처리한 상태. Fix, To-DO!
-
 #include "AssetManager.h"
 #include "CoreMain.h"
-
-#include "../ParagonGraphics/GraphicsMain.h"
-#include "../ParagonGameEngine/EngineMain.h"
 
 #include "../ParagonGraphics/GraphicsResourceManager.h"
 #include "../ParagonGameEngine/EngineResourceManager.h"
@@ -23,179 +18,20 @@
 #pragma comment(lib,"..\\Builds\\x64\\Release\\ParagonUtil.lib")
 #endif // _DEBUG
 
-#ifdef _DEBUG
-#pragma comment(lib,"..\\Builds\\x64\\Debug\\ParagonGameEngine.lib")
-#else
-#pragma comment(lib,"..\\Builds\\x64\\Release\\ParagonGameEngine.lib")
-#endif // _DEBUG
-
-#ifdef _DEBUG
-#pragma comment(lib,"..\\Builds\\x64\\Debug\\ParagonGraphics.lib")
-#else
-#pragma comment(lib,"..\\Builds\\x64\\Release\\ParagonGraphics.lib")
-#endif // _DEBUG
-
 namespace Pg::Core::Manager
 {
-	AssetManager::AssetManager() :
-		_engineResourceManager(nullptr), _graphicsResourceManager(nullptr)
+	AssetManager::AssetManager()
 	{
 		//
 	}
 
-	void AssetManager::Initialize(Pg::Core::CoreMain* core)
+	void AssetManager::Initialize(Pg::Core::CoreMain* core) : _coreMain(core)
 	{
-		IEngine* tEngine = core->_engine.get();
-		IGraphics* tGraphics = core->_graphics.get();
-
-		Pg::Engine::EngineMain* tEngineMain = static_cast<Pg::Engine::EngineMain*>(tEngine);
-		Pg::Graphics::GraphicsMain* tGraphicsMain = static_cast<Pg::Graphics::GraphicsMain*>(tGraphics);
-
-		assert(tEngineMain);
-		assert(tGraphicsMain);
-
-		this->_engineResourceManager = tEngineMain->GetEngineResourceManager();
-		this->_graphicsResourceManager = tGraphicsMain->GetGraphicsResourceManager();
+		
 	}
 
 	AssetManager::~AssetManager()
 	{
 
 	}
-
-	//원래는 Templated Class라 Header로 가야 하지만, CPP Forward Declaration 방법으로 문제 해결.
-	template<typename T>
-	void Pg::Core::Manager::AssetManager::Load(const std::string& filepath)
-	{
-		static_assert(std::is_base_of<BaseResource, T>::value, "T는 BaseResource의 자식이어야 함!");
-		static_assert(!std::is_same<BaseResource, T>::value, "T는 BaseResource 자체가 될 수는 없습니다.");
-		static_assert(!std::is_same<EngineResource, T>::value, "T는 EngineResource 자체가 될 수는 없습니다.");
-		static_assert(!std::is_same<GraphicsResource, T>::value, "T는 GraphicsResource 자체가 될 수는 없습니다.");
-
-		//Path 표기 방식 일원화.
-		std::string path = Pg::Util::Helper::ResourceHelper::ForcePathUniform(filepath);
-
-		std::shared_ptr<T> res = nullptr;
-
-		//무조건적으로 해당되는 리소스값이 반환되는지 체크한다.
-		bool tAssureGot = false;
-
-		//일단 Resource가 String 형태로 저장되어 있는지 확인.
-		if (this->_resources.contains(path))
-		{
-			//이미 리소스가 목록에 있는 상황이다. 그냥 리턴해도 상관X.
-			return;
-		}
-		else
-		{
-			//String 목록에 없다. 새로 만들어야 하는 상황!
-
-			// Engine / Graphics 나눠서 실행한다.
-			if constexpr (std::is_base_of<EngineResource, T>::value && (!std::is_base_of<GraphicsResource, T>::value))
-			{
-				//EngineResourceManager의 리소스를 가져오는 로직.
-				res = _engineResourceManager->CreateResource<T>(path);
-
-				tAssureGot = true;
-			}
-
-			if constexpr ((!std::is_base_of<EngineResource, T>::value) && std::is_base_of<GraphicsResource, T>::value)
-			{
-				//GraphicsResourceManager의 리소스를 가져오는 로직.
-				res = _graphicsResourceManager->CreateResource<T>(path);
-				tAssureGot = true;
-			}
-
-			assert(tAssureGot && "T는 EngineResource도, GraphicsResource도 아니다.");
-		}
-
-		//성공했으니, 메인 관리 리스트에 Path String 추가.
-		_resources.insert(path);
-	}
-
-	//원래는 Templated Class라 Header로 가야 하지만, CPP Forward Declaration 방법으로 문제 해결.
-	template<typename T>
-	std::shared_ptr<T>
-		Pg::Core::Manager::AssetManager::Get(const std::string& filepath)
-	{
-		static_assert(std::is_base_of<BaseResource, T>::value, "T는 BaseResource의 자식이어야 함!");
-		static_assert(!std::is_same<BaseResource, T>::value, "T는 BaseResource 자체가 될 수는 없습니다.");
-		static_assert(!std::is_same<EngineResource, T>::value, "T는 EngineResource 자체가 될 수는 없습니다.");
-		static_assert(!std::is_same<GraphicsResource, T>::value, "T는 GraphicsResource 자체가 될 수는 없습니다.");
-
-		//Path 표기 방식 일원화.
-		std::string path = Pg::Util::Helper::ResourceHelper::ForcePathUniform(filepath);
-
-		std::shared_ptr<T> res = nullptr;
-
-		//무조건적으로 해당되는 리소스값이 반환되는지 체크한다.
-		bool tAssureGot = false;
-
-		//일단 Resource가 String 형태로 저장되어 있는지 확인.
-		if (this->_resources.contains(path))
-		{
-			//이미 정보가 있다. 이 정보만을 가져와서 출력하면 되는 것!
-
-			// Engine / Graphics 나눠서 실행한다.
-			if constexpr (std::is_base_of<EngineResource, T>::value && (!std::is_base_of<GraphicsResource, T>::value))
-			{
-				//EngineResourceManager의 리소스를 가져오는 로직.
-				res = _engineResourceManager->GetResource<T>(path);
-				tAssureGot = true;
-			}
-
-			if constexpr ((!std::is_base_of<EngineResource, T>::value) && std::is_base_of<GraphicsResource, T>::value)
-			{
-				//GraphicsResourceManager의 리소스를 가져오는 로직.
-				res = _graphicsResourceManager->GetResource<T>(path);
-				tAssureGot = true;
-			}
-
-			assert(tAssureGot && "T는 EngineResource도, GraphicsResource도 아니다.");
-		}
-		else
-		{
-			//Get하는 시점에서 리소스는 없으면 안된다.
-
-			assert(false && "AssetManager에서, 없는 리소스를 Get하려고 함!");
-		}
-
-		//성공했으니, 메인 관리 리스트에 Path String 추가.
-		_resources.insert(path);
-
-		//이미 다이내믹 캐스트로 값이 들어온 상황.
-		return res;
-	}
-
-	void AssetManager::Unload(const std::string& filepath)
-	{
-		//Path 표기 방식 일원화.
-		std::string path = Pg::Util::Helper::ResourceHelper::ForcePathUniform(filepath);
-
-		//우선적으로 일단 리스트에 값이 있는지를 확인.
-		if (this->_resources.contains(path))
-		{
-			//명시적으로 두 인스턴스에 모두 삭제(및 언로드)를 진행해준다. 
-			//템플릿을 쓰지 않기 위해, 둘 다 삭제 코드, 내부적으로 해당하는지 아닌지 반환.
-			
-			bool tIsEngineDelete = _engineResourceManager->DeleteResource(path);
-			bool tIsGraphicsDelete = _graphicsResourceManager->DeleteResource(path);
-			assert((tIsEngineDelete ^ tIsGraphicsDelete) && "하나만 삭제하는 경우가 발생하지 않음, 오류");
-		
-			//이제 메인 AssetManager에서 값을 삭제.
-			_resources.erase(path);
-		}
-		else
-		{
-			//리스트에 값이 없다는 뜻이다. 언로드될 수도 없음.
-			return;
-		}
-	}
-
-	///실제로 AssetManager에서 템플릿 인스턴스화될 클래스들은 CPP에서 전방참조된다.
-	///List가 추가될 수록, 컴파일러가 알아차릴 수 있게 목록이 추가되어야. 
-	///오류가 나지 않게, 실제로 쓰이는 코드들만 CPP에서 전방선언되어야 한다.
-	//template void AssetManager::Load<Pg::Graphics::RenderMaterial>(const std::string&);
-	//template std::shared_ptr<Pg::Graphics::RenderMaterial> 
-	//	AssetManager::Get<Pg::Graphics::RenderMaterial>(const std::string&);
 }
--- ParagonEngine/ParagonCore/AssetManager.h
+++ ParagonEngine/ParagonCore/AssetManager.h
@@ -1,39 +1,15 @@
 #pragma once
-#include "CoreSingleton.h"
-#include "BaseResource.h"
-#include "EngineResource.h"
-#include "GraphicsResource.h"
+#include "AssetDefines.h"
 
 #include <string>
-#include <memory>
-#include <set>
-#include <stdexcept>
-#include <type_traits>
-#include <cassert>
+#include <unordered_map>
+#include <vector>
 
 /// <summary>
 /// 게임 엔진 내의 모든 애셋/리소스 내용을 총괄할 매니저.
-/// 
-/// CAUTION:
-/// Asset Load / Unload를 하기 위해서는 반드시 본 함수로 실행해야 한다. 
-/// Graphics/Engine ResourceManager 직접 접근 금지.
-/// Asset관리를 받을 대상은 용도에 따라 EngineResource / GraphicsResource를 상속받아야 하고,
-/// 하위 리소스들은 내부적으로 반드시 InternalLoad / InternalUnload 함수가 구현되어 있어야 한다.
-/// 리소스 로드와 가져오는 시점은 분리되어 있다.
-/// 
-/// NOTES:
-/// AssetManager는 Graphics, Engine을 알아야 하는 상황인데, 
-/// -> 다른 것도 아니고 Unload만 오류가 났던 이유는
-/// 다른 Get / Create는 template function들이라 쓰일 때 오류를 일으키기 때문이다.
-/// 이렇게 된다면, 개별적 리소스 매니저에 있는 함수들이 독립적으로 호출되어야 하는가 ?
-/// 그러면 로직이 관리되지 않는다.
-/// 이렇게 된다면, AssetManager 관리 로직이 바뀌어야 한다.
-/// Core의 AssetManager가 Graphics, Engine을 아는 것은 변하지 않는다.
-/// 다만, 더 이상 Graphics, Engine 내부에서 AssetManager를 통해서 리소스에 접근한다는 생각을 버리고,
-/// GameEngine, Graphics에서 자체 리소스매니저를 통해서 접근한다는 생각을 해야 한다는 것인데..
-/// 
-/// Template Class Forward Declaration 방법으로 해결된다면, 
-/// 사용되는 모든 저장 클래스들은 CPP 하단부에 정의되어야 한다.
+/// 실질적인 기능은 Engine, Graphics의 하위 리소스 매니저들이 담당하고, 
+/// 본 AssetManager의 역할은 일종의 공유되는 장부와 비슷하다.
+/// singleton-cpp로 인스턴스가 공유된다.
 /// </summary>
 
 namespace Pg::Core
@@ -41,30 +17,9 @@ namespace Pg::Core
 	class CoreMain;
 }
 
-namespace Pg::Engine
-{
-	namespace Manager
-	{
-		class EngineResourceManager;
-	}
-}
-
-namespace Pg::Graphics
-{
-	namespace Manager
-	{
-		class GraphicsResourceManager;
-	}
-	
-}
-
 namespace Pg::Core::Manager
 {
-	using Pg::Core::Resources::BaseResource;
-	using Pg::Core::Resources::GraphicsResource;
-	using Pg::Core::Resources::EngineResource;
-
-	class AssetManager : public Pg::Core::Singleton<AssetManager>
+	class AssetManager
 	{
 	public:
 		AssetManager();
@@ -73,23 +28,23 @@ namespace Pg::Core::Manager
 		//AssetManager 초기화. Engine/Graphics의 리소스 매니저를 받아온다.
 		void Initialize(Pg::Core::CoreMain* core);
 
-		//리소스를 실제로 컨테이너에 로드하는 함수, 로드만 할 뿐이지 함수 자체가 리소스를 반환하지는 않는다.
-        template<typename T>
-		void Load(const std::string& filepath);
-        
-		//리소스를 언로드하는 함수.
-		void Unload(const std::string& filepath);
-		
-		//리소스를 실제로 반환받아서 쓸 수 있게 하는 함수. 
-		template<typename T>
-		std::shared_ptr<T> Get(const std::string& filepath);
+		//실제로 세부 Graphics, Engine의 리소스 매니저를 조종하기 위해, 로직 업데이트.
+		void Update();
+
+		//리소스가 실제로 중앙 AssetManager 안에 존재하는지를 판단해준다.
+		void IsExistResource(Pg::Core::Enums::eAssetDefine define, const std::string& filepath);
+
+		//리소스가 
+		void LoadResource(Pg::Core::Enums::eAssetDefine define, const std::string& filepath);
+
+		void UnloadResource(const std::string& filepath);
 
 	private:
-		std::set<std::string> _resources;
+		Pg::Core::CoreMain* _coreMain;
 
-	private:
-		//별도로 각가 GameEngine / Graphics 프로젝트에서 만들어졌던 리소스 매니저.
-		Pg::Engine::Manager::EngineResourceManager* _engineResourceManager;
-		Pg::Graphics::Manager::GraphicsResourceManager* _graphicsResourceManager;
+		//실제로 보관하는 리소스 맵. <파일 경로 / Asset의 종류>
+		std::unordered_map<std::string, Pg::Core::Enums::eAssetDefine> _resourceMap;
+
+		std::vector<std
 	};
 }
