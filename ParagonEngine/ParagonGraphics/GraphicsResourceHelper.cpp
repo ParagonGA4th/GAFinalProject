6acf076b5384a62ae6c2fc1ab9f0230084829217
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2023-09-30 16:34:49 +0900

    [고태욱] 개인 브랜치, 프로젝트 연결 관계 with 기존 구조 강화 리팩토링 진행중.

---
+++ ParagonEngine/ParagonGraphics/GraphicsResourceHelper.cpp
@@ -0,0 +1,1 @@
+#include "GraphicsResourceHelper.h"
--- ParagonEngine/ParagonUtil/ResourceHelper.cpp
+++ ParagonEngine/ParagonUtil/ResourceHelper.cpp
@@ -1,10 +1,11 @@
 #include "ResourceHelper.h"
 #include <algorithm>
 #include <filesystem>
+#include <cassert>
 
 namespace Pg::Util::Helper
 {
-	using Pg::Core::Enums::eResourceType;
+	using Pg::Core::Enums::eAssetDefine;
 
 	ResourceHelper::ResourceHelper()
 	{
@@ -22,6 +23,35 @@ namespace Pg::Util::Helper
 		return tPath.generic_string();
 	}
 
-	
+	bool ResourceHelper::IsGraphicsResource(Pg::Core::Enums::eAssetDefine define)
+	{
+		bool tIsGraphicsResource = false;
+
+		switch (define)
+		{
+		case eAssetDefine::_NONE:
+		{
+			assert(false);
+		}
+		break;
+		case eAssetDefine::_2DTEXTURE:		[[fallthrough]];
+		case eAssetDefine::_CUBEMAP:		[[fallthrough]];
+		case eAssetDefine::_3DSTATICMODEL:	[[fallthrough]];
+		case eAssetDefine::_3DSKINNEDMODEL: [[fallthrough]];
+		case eAssetDefine::_FONT:			[[fallthrough]];
+		case eAssetDefine::_RENDERSHADER:   [[fallthrough]];
+		case eAssetDefine::_RENDERMATERIAL:
+			tIsGraphicsResource = true;
+			break;
+		default:
+		{
+			assert(false);
+		}
+		break;
+		}
+
+		return tIsGraphicsResource;
+	}
+
 
 }
