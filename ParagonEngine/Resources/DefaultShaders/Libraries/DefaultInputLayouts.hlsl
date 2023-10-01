95de47bd775bcd9124ced7cd342f97e7f8382ba8
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2023-10-01 13:01:26 +0900

    [고태욱] 개인 브랜치 커밋, 그래픽 엔진의 1st 레이어 준비 중, 기본 Vertex Shader Static / Skinned 코드 작성, Include 관계 정리.

---
+++ ParagonEngine/Resources/DefaultShaders/Libraries/DefaultInputLayouts.hlsl
@@ -0,0 +1,46 @@
+//그래픽 엔진 디폴트 InputLayout을 저장한 HLSL.
+
+#ifndef __DEFINED_DEFAULT_INPUTLAYOUTS_HLSL__
+#define __DEFINED_DEFAULT_INPUTLAYOUTS_HLSL__
+
+struct Vin1stStatic
+{
+    float3 vin1st_PosL : POSITION;
+    float3 vin1st_NormalL : NORMAL;
+    float3 vin1st_TangentL : TANGENT;
+    float4 vin1st_Color : COLOR;
+    float3 vin1st_Tex : TEXCOORD;
+    //uint vin1st_MatID : MATERIALID;
+};
+
+struct Vin1stSkinned
+{
+    float3 vin1st_PosL : POSITION;
+    float3 vin1st_NormalL : NORMAL;
+    float3 vin1st_TangentL : TANGENT;
+    float4 vin1st_Color : COLOR;
+    float3 vin1st_Tex : TEXCOORD;
+    //uint vin1st_MatID : MATERIALID;
+    
+    uint vin1st_BlendIndice0 : BLENDINDICES0;
+    uint vin1st_BlendIndice1 : BLENDINDICES1;
+    uint vin1st_BlendIndice2 : BLENDINDICES2;
+    uint vin1st_BlendIndice3 : BLENDINDICES3;
+    
+    float vin1st_BlendWeight0 : BLENDWEIGHT0;
+    float vin1st_BlendWeight1 : BLENDWEIGHT1;
+    float vin1st_BlendWeight2 : BLENDWEIGHT2;
+};
+
+struct VOut1st
+{
+    float4 vout1st_PosH : SV_POSITION;
+    float3 vout1st_PosW : POSITION;
+    float3 vout1st_NormalW : NORMAL;
+    float3 vout1st_TangentW : TANGENT;
+    float4 vout1st_Color : COLOR;
+    float3 vout1st_Tex : TEXCOORD;
+    //uint vout1st_MatID : MATERIALID;
+};
+
+#endif //__DEFINED_DEFAULT_INPUTLAYOUTS_HLSL__
