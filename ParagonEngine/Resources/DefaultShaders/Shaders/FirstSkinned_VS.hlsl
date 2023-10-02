272ce62230861ce1b4596c493093cafcb1f74a54
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2023-10-02 11:37:33 +0900

    [고태욱] 개인 브랜치 작업, 1st Stage Pixel Shader 작성 완료 / 그래픽 엔진 구축 중 (1차)

--- ParagonEngine/Resources/DefaultShaders/Shaders/FirstSkinned_VS.hlsl
+++ ParagonEngine/Resources/DefaultShaders/Shaders/FirstSkinned_VS.hlsl
@@ -40,7 +40,8 @@ VOut1st VS_MAIN(Vin1stSkinned input)
 	// Color & UV(W) 값 전달.
     output.vout1st_Color = input.vin1st_Color;
     output.vout1st_Tex = input.vin1st_Tex;
-	//추후에 Material ID가 로직에 들어가면 이 역시 추가해야 한다.
+	//추후에 Material ID가 로직에 들어가면 이 역시 하드코딩에서 바꿔야 한다.
+    output.vout1st_MatID = 0;
     
     return output;
 }
