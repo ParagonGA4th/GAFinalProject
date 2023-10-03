7cfd9327a8e1cf75e6e4459b1262b4f7872ebd42
Min-seo-Choi <Min-seo-Choi@users.noreply.github.com>
2023-10-03 18:01:54 +0900

    [최민서] xml data load

--- ParagonEngine/ParagonEditor/FileManager.cpp
+++ ParagonEngine/ParagonEditor/FileManager.cpp
@@ -8,57 +8,78 @@ void FileManager::XmlLoad()
 	if (doc.load_file("SceneName.xml"))
 	{
 		rootNode = doc.child("scene");
-		GameObjectData obj;
-		XmlDataParsing(rootNode.first_child(), obj);
+		XmlDataParsing(rootNode.first_child());
 	}
 }
 
-void FileManager::XmlDataParsing(pugi::xml_node node, GameObjectData& data)
+void FileManager::JsonLoad()
 {
-	std::string str;
-	std::string nodeName = node.name();
+}
 
-	if (nodeName.compare("objects") == 0) XmlDataParsing(node.first_child(), data);
+void FileManager::XmlDataParsing(pugi::xml_node node)
+{
+	// node Name = objects
 
-	for (pugi::xml_node child = node.first_child(); child; child = child.next_sibling()) 
+	for (pugi::xml_node child = node.first_child(); child; child = child.next_sibling())
 	{
-		str = child.name();
+		GameObjectData objData;
 
-		if ( str.compare("components") == 0
-			|| str.compare("component") == 0 || str.compare("data") == 0) {	XmlDataParsing(child, data);}
+		// object 안에 들어있는 데이터를 
+		XmlObjectDataParsing(child, objData);
 
-		if (str.compare("name") == 0) { data._name = child.child_value(); }
-		if (str.compare("tag") == 0) { data._tag = child.child_value(); }
-		if (str.compare("active") == 0) { data._active = child.child_value(); }
-		if (str.compare("parent") == 0) { data._parent = std::stoi(child.child_value()); }
-		if (str.compare("type") == 0) { data._type = child.child_value(); }
-
-		if (str.compare("position") == 0)
-		{
-			data._transform._position._x = std::stof(child.first_child().child_value());
-			data._transform._position._y = std::stof(child.first_child().next_sibling().child_value());
-			data._transform._position._z = std::stof(child.first_child().next_sibling().next_sibling().child_value());
-		}		
-		
-		if (str.compare("rotation") == 0)
-		{
-			data._transform._rotation._x = std::stof(child.first_child().child_value());
-			data._transform._rotation._y = std::stof(child.first_child().next_sibling().child_value());
-			data._transform._rotation._z = std::stof(child.first_child().next_sibling().next_sibling().child_value());
-		}		
-		
-		if (str.compare("scale") == 0)
-		{
-			data._transform._scale._x = std::stof(child.first_child().child_value());
-			data._transform._scale._y = std::stof(child.first_child().next_sibling().child_value());
-			data._transform._scale._z = std::stof(child.first_child().next_sibling().next_sibling().child_value());
-		}
+		_gameObjectDatas.push_back(objData);
 	}
+}
 
-	if (!isDataPushing)
+void FileManager::XmlObjectDataParsing(pugi::xml_node node, GameObjectData& data)
+{
+	// Object가 넘어온다
+	for (pugi::xml_node child = node.first_child(); child; child = child.next_sibling())
 	{
-		_gameObjectDatas.push_back(data);
-		isDataPushing = true;
-		return;
+		std::string childName = child.name();
+
+		if (childName.compare("name") == 0) { data._name = child.child_value(); }
+		if (childName.compare("tag") == 0) { data._tag = child.child_value(); }
+		if (childName.compare("active") == 0) { data._active = child.child_value(); }
+		if (childName.compare("parent") == 0) { data._parent = std::stoi(child.child_value()); }
+		if (childName.compare("components") == 0) { XmlComponentDataParsing(child.first_child(), data); }
+	}
+}
+
+void FileManager::XmlComponentDataParsing(pugi::xml_node node, GameObjectData& data)
+{
+	for (pugi::xml_node child = node.first_child(); child; child = child.next_sibling())
+	{
+		std::string childName = child.name();
+
+		if (childName.compare("type") == 0) { data._type = child.child_value(); }
+		if (childName.compare("data") == 0) 
+		{ 
+			for (pugi::xml_node childData = child.first_child(); childData; childData = childData.next_sibling())
+			{
+				std::string childName = childData.name();
+
+				if (childName.compare("position") == 0) 
+				{
+					data._transform._position._x = std::stof(childData.first_child().child_value());
+					data._transform._position._y = std::stof(childData.first_child().next_sibling().child_value());
+					data._transform._position._z = std::stof(childData.first_child().next_sibling().next_sibling().child_value());
+				}
+
+				if (childName.compare("rotation") == 0) 
+				{
+					data._transform._rotation._x = std::stof(childData.first_child().child_value());
+					data._transform._rotation._y = std::stof(childData.first_child().next_sibling().child_value());
+					data._transform._rotation._z = std::stof(childData.first_child().next_sibling().next_sibling().child_value());
+				}
+
+				if (childName.compare("scale") == 0) 
+				{
+					data._transform._scale._x = std::stof(childData.first_child().child_value());
+					data._transform._scale._y = std::stof(childData.first_child().next_sibling().child_value());
+					data._transform._scale._z = std::stof(childData.first_child().next_sibling().next_sibling().child_value());
+				}
+			}
+		}
 	}
 }
