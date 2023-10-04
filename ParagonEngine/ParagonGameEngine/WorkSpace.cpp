5bb9093eef03fe27eab3bba765f3d1e7ae49db37
Jisang Byeon <pannasonic74@gmail.com>
2023-10-04 18:56:18 +0900

    [변지상] WorkSpace에 디버그로그 추가

--- ParagonEngine/ParagonGameEngine/WorkSpace.cpp
+++ ParagonEngine/ParagonGameEngine/WorkSpace.cpp
@@ -1,6 +1,15 @@
 #include "WorkSpace.h"
 #include "TestScene.h"
+#include "../ParagonUtil/Log.h"
+
 #include <windows.h>
+
+#ifdef _DEBUG
+#pragma comment(lib,"..\\Builds\\x64\\Debug\\ParagonUtil.lib")
+#else
+#pragma comment(lib,"..\\Builds\\x64\\Release\\ParagonUtil.lib")
+#endif // _DEBUG
+
 
 namespace Pg::Engine
 {
@@ -8,7 +17,7 @@ namespace Pg::Engine
 	{
 		_testScene = new TestScene();
 		_testScene->Initialize();
-		OutputDebugString(L"Enter WorkSpace!");
+		PG_TRACE("Enter WorkSpace!");
 	}
 
 	Pg::Core::Scene* WorkSpace::GetCurrentScene()
