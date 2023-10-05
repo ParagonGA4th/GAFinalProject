7cf50eaad36b327071a6f9a7211a27787e9dcb74
Minseo Choi <78577815+MinseoChoi03@users.noreply.github.com>
2023-10-05 13:58:59 +0900

    [최민서] Main과 병합 하기 전 커밋 / InputText가 작동이 안되는 상태..

--- ParagonEngine/ParagonEditor/FileManager.h
+++ ParagonEngine/ParagonEditor/FileManager.h
@@ -11,12 +11,14 @@ public:
 	void XmlLoad();
 	void JsonLoad();
 
+	std::vector<GameObjectData*> GetGameObjectData() const;
+
 private:
 	void XmlDataParsing(pugi::xml_node node);
 	void XmlObjectDataParsing(pugi::xml_node node, GameObjectData& data);
 	void XmlComponentDataParsing(pugi::xml_node node, GameObjectData& data);
 
 private:
-	std::vector<GameObjectData> _gameObjectDatas;
+	std::vector<GameObjectData*> _gameObjectDatas;
 };
 
