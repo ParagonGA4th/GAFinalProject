c49d59952afce390f0e83672138254f90b091d2b
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2023-10-05 14:26:23 +0900

    [고태욱] 리팩토링 전, 빌드 가능하게 PhysX 임시로 제외

--- ParagonEngine/ParagonAPI/PhysXTest.h
+++ ParagonEngine/ParagonAPI/PhysXTest.h
@@ -1,16 +1,16 @@
-#pragma once
-#include "../lib/PhysX/physx/installed/x64-windows/include/PxPhysics.h"
-#include "../lib/PhysX/physx/installed/x64-windows/include/PxPhysicsAPI.h"
-
-/// <summary>
-/// PhysX 초기화를 위한 테스트 클래스.
-/// 2023.10.04
-/// </summary>
-namespace Pg::API
-{
-	class PhysXTest
-	{
-	public:
-		void Initialize();
-	};
-}
+//#pragma once
+//#include "../lib/PhysX/physx/installed/x64-windows/include/PxPhysics.h"
+//#include "../lib/PhysX/physx/installed/x64-windows/include/PxPhysicsAPI.h"
+//
+///// <summary>
+///// PhysX 초기화를 위한 테스트 클래스.
+///// 2023.10.04
+///// </summary>
+//namespace Pg::API
+//{
+//	class PhysXTest
+//	{
+//	public:
+//		void Initialize();
+//	};
+//}
