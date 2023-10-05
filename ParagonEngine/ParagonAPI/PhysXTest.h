596145e224b32efe0893ff73f7b0ba3f13155004
Jisang Byeon <pannasonic74@gmail.com>
2023-10-05 14:29:28 +0900

    [변지상] 별거 없는 커밋

--- ParagonEngine/ParagonAPI/PhysXTest.h
+++ ParagonEngine/ParagonAPI/PhysXTest.h
@@ -1,6 +1,5 @@
 #pragma once
-#include "../lib/PhysX/physx/installed/x64-windows/include/PxPhysics.h"
-#include "../lib/PhysX/physx/installed/x64-windows/include/PxPhysicsAPI.h"
+
 
 /// <summary>
 /// PhysX 초기화를 위한 테스트 클래스.
