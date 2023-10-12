4f4eb88836ebb19848347742242a3fddfbd64881
Min-seo-Choi <mschoi0814@gmail.com>
2023-10-12 16:52:40 +0900

    [최민서] 다시 client와 editor 분리, 구조 조정 및 세분화

--- ParagonEngine/ParagonEditor/EditorManager.h
+++ ParagonEngine/ParagonEditor/EditorManager.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <windows.h>
 #include <memory>
 #include <gainput/gainput.h>
 
@@ -14,25 +15,33 @@ namespace Pg::API::Input
 
 namespace Pg::Editor::Manager
 {
+	class UIManager;
+
 	class EditorManager
 	{
 	public:
 		EditorManager();
 		~EditorManager();
 		
-		void Initialize(void* hWnd, float width, float height);
+		void Initialize(HWND hWnd, float width, float height);
 		void Update();
 
 		// Graphics 관련, 화면의 크기가 재정의 되었을 때 사용되는 함수
-		void ReSizeHandler(float width, float height);
+		//void ReSizeHandler(float width, float height);
 
-		void InputHandler(MSG message);
+		// Input 관련, 입력 받기 위해 window의 message를 받아야함
+		//void InputHandler(MSG message);
+
+		// Imgui 관련, 입력 등 관련 기능을 사용하려면 WndProc에 있어야 함
+		void Handler(MSG message);
 		
 		void Finalize();
 
 	private:
 		std::unique_ptr<Pg::Core::ProcessMain> _coreMain;
 		Pg::API::Input::PgInput* _input;
+
+		std::unique_ptr<Pg::Editor::Manager::UIManager> _UIManager;
 
 	private:
 		bool _isCoreInitailized;
