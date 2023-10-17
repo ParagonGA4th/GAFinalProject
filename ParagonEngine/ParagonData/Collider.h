b615a8c9eb71848c9901155d8abc53b0aaf62dac
Jisang Byeon <pannasonic74@gmail.com>
2023-10-17 15:49:10 +0900

    [변지상] PhysX브랜치 전 커밋

--- ParagonEngine/ParagonData/Collider.h
+++ ParagonEngine/ParagonData/Collider.h
@@ -1,5 +1,27 @@
 #pragma once
-class Collider
+#include "Component.h"
+#include "GameObject.h"
+
+/// <summary>
+/// 변지상의 Collider 컴포넌트
+/// 2D와 3D에 플래그 적용하여 오브젝트에 맞는 Collider 적용할 예정.
+/// 근데 일단 PhysX부터 어찌.....
+/// 2023.10.17
+/// </summary>
+
+namespace PG::Data
 {
-};
+	class GameObject;
+}
+
+namespace Pg::Data
+{
+	class Collider : public Component
+	{
+	public:
+		Collider();
+
+	private:
+	};
+}
 
