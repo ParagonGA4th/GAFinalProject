3eb55dbf178c119ce605f3eeae9380914bc479ce
Min-seo-Choi <mschoi0814@gmail.com>
2023-10-18 16:41:56 +0900

    [최민서] Editor와 UI(Imgui) 분리 이제 구조(Panel, Widget 등) 잡아야 함

---
+++ ParagonEngine/ParagonUI/ImguiManager.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <windows.h>
+
+struct ID3D11Device;
+struct ID3D11DeviceContext;
+
+namespace Pg::UI::Manager
+{
+	class ImGuiManager
+	{
+	public:
+		ImGuiManager();
+		~ImGuiManager();
+
+		void Initialize(void* hWnd, ID3D11Device* device, ID3D11DeviceContext* deviceContext);
+		void CreateFrame();
+		void Render();
+		void Finalize();
+
+		void ImguiHandler(MSG message);
+	};
+}
+
