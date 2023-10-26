94ab4fdbd95800dce87ada20e0d0951730b45a80
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2023-10-26 12:25:54 +0900

    [고태욱] 일부 안쓰이는 FBX 제거 / 예시용 FBX에서 안 쓰이는 FBX 제거 // 일부 추가 (Cone 텍스쳐 변경) // Git LFS FBX Track 추가.

--- ParagonEngine/ParagonData/StaticMeshRenderer.cpp
+++ ParagonEngine/ParagonData/StaticMeshRenderer.cpp
@@ -1,11 +1,23 @@
 #include "StaticMeshRenderer.h"
+#include "../ParagonUtil/ResourceHelper.h"
 
 namespace Pg::Data
 {
+	using Pg::Util::Helper::ResourceHelper;
+
 	StaticMeshRenderer::StaticMeshRenderer(GameObject* obj) : RendererBase3D(obj)
 	{
 		SetRendererTypeName(typeid(this).name());
 	}
 
+	void StaticMeshRenderer::SetMeshFilePath(const std::string& meshFilePath)
+	{
+		this->_meshFilePath = ResourceHelper::ForcePathUniform(meshFilePath);
+	}
+
+	std::string StaticMeshRenderer::GetMeshFilePath()
+	{
+		return _meshFilePath;
+	}
 
 }
