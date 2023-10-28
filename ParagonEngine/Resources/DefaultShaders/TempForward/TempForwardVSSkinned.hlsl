d3f8b799c61fd56f43ff19c86267c0ce52d48bfd
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2023-10-28 20:29:24 +0900

    [고태욱] 스크린에 알맞게 노드 Transform을 반영해서 제대로 모델 출력 자체에는 성공 // 1프레임 단위로 나오는 찢기는 버퍼 // 다른 옵젝과 비교해서 1/100 작은 WorldMat / WorldMat 3행4열 w값 0이 아니게 된 문제 발견.

--- ParagonEngine/Resources/DefaultShaders/TempForward/TempForwardVSSkinned.hlsl
+++ ParagonEngine/Resources/DefaultShaders/TempForward/TempForwardVSSkinned.hlsl
@@ -46,18 +46,17 @@ struct VOut1st
 VOut1st VS_MAIN(Vin1stSkinned input)
 {
     VOut1st output;
-	
-    
-	//Skinning 과정.
-    float _weights[4] = { 0.f, 0.f, 0.f, 0.f };
-    _weights[0] = input.vin1st_BlendWeight0;
-    _weights[1] = input.vin1st_BlendWeight1;
-    _weights[2] = input.vin1st_BlendWeight2;
-    _weights[3] = 1 - (_weights[0] + _weights[1] + _weights[2]);
 
-    //TEMP
-    float3 skinnedPosL = float3(1.f, 1.f, 1.f);
-    float3 skinnedNormalL = float3(1.f, 1.f, 1.f);
+	////Skinning 과정.
+    //float _weights[4] = { 0.f, 0.f, 0.f, 0.f };
+    //_weights[0] = input.vin1st_BlendWeight0;
+    //_weights[1] = input.vin1st_BlendWeight1;
+    //_weights[2] = input.vin1st_BlendWeight2;
+    //_weights[3] = 1 - (_weights[0] + _weights[1] + _weights[2]);
+    //
+    ////TEMP
+    //float3 skinnedPosL = float3(1.f, 1.f, 1.f);
+    //float3 skinnedNormalL = float3(1.f, 1.f, 1.f);
     
     //float3 skinnedPosL = float3(0.f, 0.f, 0.f);
     //float3 skinnedNormalL = float3(0.f, 0.f, 0.f);
@@ -74,18 +73,16 @@ VOut1st VS_MAIN(Vin1stSkinned input)
     //skinnedPosL += _weights[3] * mul(float4(input.vin1st_PosL, 1.f), gCBuf_Bones[input.vin1st_BlendIndice3]).xyz;
     //skinnedNormalL += _weights[3] * mul(input.vin1st_NormalL, (float3x3) gCBuf_Bones[input.vin1st_BlendIndice3]);
 	
-	// Position을 Local -> World 이동.
-    output.vout1st_PosW = mul(float4(skinnedPosL, 1.0f), gCBuf_World).xyz;
-	
-	// Normal을 Local -> World 이동.
-    output.vout1st_NormalW = mul(skinnedNormalL, (float3x3) gCBuf_WorldInvTranspose);
+    output.vout1st_PosW = mul(float4(input.vin1st_PosL, 1.0f), gCBuf_World).xyz;
+    // Normal을 Local -> World 이동.
+    output.vout1st_NormalW = mul(input.vin1st_NormalL, (float3x3) gCBuf_WorldInvTranspose);
 	
 	// Tangent를 Local -> World 이동.
     output.vout1st_TangentW = mul(input.vin1st_TangentL, (float3x3) gCBuf_World);
 	
 	// 동차좌표계 내 Position 계산.
-    output.vout1st_PosH = mul(float4(skinnedPosL, 1.0f), gCBuf_WorldViewProj);
-	
+    output.vout1st_PosH = mul(gCBuf_WorldViewProj, float4(input.vin1st_PosL, 1.0f));
+    
 	// Color & UV(W) 값 전달.
     output.vout1st_Color = input.vin1st_Color;
     output.vout1st_Tex = input.vin1st_Tex;
@@ -96,4 +93,3 @@ VOut1st VS_MAIN(Vin1stSkinned input)
     
     return output;
 }
-
