8318762af9c1557b762ba44e80fe865da8b338b8
Min-seo-Choi <mschoi0814@gmail.com>
2023-11-02 12:36:58 +0900

    [최민서] ParagonUI 의 함수를 사용해서 만든 imgui UI

---
+++ ParagonEngine/ParagonEditor/Scene.cpp
@@ -0,0 +1,53 @@
+#include "Scene.h"
+#include "EditorHelper.h"
+#include "../ParagonUI/UIManager.h"
+#include "../ParagonUI/WidgetContainer.h"
+#include <singleton-cpp/singleton.h>
+
+#include "../ParagonUI/Image.h"
+
+Pg::Editor::Window::Scene::Scene()
+	:_winName("Scene"), _isShow(true)
+{
+	auto& tedhepler = singleton<Pg::Editor::Helper::EditorHelper>();
+	_editorHelper = &tedhepler;
+	
+	auto& tUIManager = singleton<Pg::UI::Manager::UIManager>();
+	_uiManager = &tUIManager;
+
+	cons = new Pg::UI::WidgetContainer();
+}
+
+Pg::Editor::Window::Scene::~Scene()
+{
+
+}
+
+void Pg::Editor::Window::Scene::Initialize()
+{
+	cons->CreateWidget<Pg::UI::Widget::Image>(_editorHelper->GetTexture(), 1080.f, 920.f);
+}
+
+void Pg::Editor::Window::Scene::Update()
+{
+	_uiManager->WindowBegin(_winName);
+	cons->Update();
+	_uiManager->WindowEnd();
+}
+
+void Pg::Editor::Window::Scene::Finalize()
+{
+
+}
+
+void Pg::Editor::Window::Scene::SetShow(bool show)
+{
+	_isShow = show;
+}
+
+bool Pg::Editor::Window::Scene::GetShow()
+{
+	return _isShow;
+}
+
+
