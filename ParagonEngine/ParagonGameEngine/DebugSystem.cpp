026b37f360787191fc5d020df31aa2c61de83444
Jisang Byeon <pannasonic74@gmail.com>
2023-11-10 17:16:11 +0900

    [변지상] DebugSystem 구현 중...

---
+++ ParagonEngine/ParagonGameEngine/DebugSystem.cpp
@@ -0,0 +1,20 @@
+#include "DebugSystem.h"
+
+namespace Pg::Engine
+{
+
+	void DebugSystem::Initialize()
+	{
+
+	}
+
+	void DebugSystem::Finalize()
+	{
+
+	}
+
+	void DebugSystem::SetDebugMode(bool isdebug)
+	{
+		_isDebug = isdebug;
+	}
+}
