39265c257de9add2da17cb74295ddd777bbfc4fb
Jisang Byeon <pannasonic74@gmail.com>
2023-11-14 14:22:39 +0900

    [변지상] DebugData 헤더로 분리

--- ParagonEngine/ParagonGameEngine/DebugSystem.cpp
+++ ParagonEngine/ParagonGameEngine/DebugSystem.cpp
@@ -17,4 +17,35 @@ namespace Pg::Engine
 	{
 		_isDebug = isdebug;
 	}
+
+	void DebugSystem::DrawBoxDebug(PGFLOAT4X4 worldTM, PGFLOAT3 scale, PGFLOAT4 color)
+	{
+		if (!_isDebug)
+		{
+			return;
+		}
+
+		_boxVec.push_back({ worldTM, scale, color });
+	}
+
+	void DebugSystem::DrawLineDebug(PGFLOAT3 beginPoint, PGFLOAT3 endPoint, PGFLOAT4 color)
+	{
+
+	}
+
+	void DebugSystem::DeleteBoxDebug()
+	{
+		_boxVec.clear();
+	}
+
+	std::vector<Pg::Data::BoxInfo>& DebugSystem::GetBoxVector()
+	{
+		return _boxVec;
+	}
+
+	std::vector<Pg::Data::LineInfo>& DebugSystem::GetLineVector()
+	{
+		return _lineVec;
+	}
+
 }
