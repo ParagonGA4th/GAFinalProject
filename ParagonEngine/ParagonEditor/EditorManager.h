7cbeb46001ef0ef0cff601302439c05e7d8ed39e
Min-seo-Choi <mschoi0814@gmail.com>
2023-11-15 18:52:21 +0900

    [최민서] windowManager 생성, ui 및 window 이동

--- ParagonEngine/ParagonEditor/EditorManager.h
+++ ParagonEngine/ParagonEditor/EditorManager.h
@@ -1,20 +1,11 @@
 #pragma once
 #include <windows.h>
-#include <string>
 #include <memory>
 
-namespace Pg::UI::Manager { class UIManager; }
-namespace Pg::Editor::Helper { class EditorHelper; }
-namespace Pg::Editor::Window
-{
-	class Inspector; 
-	class Hierarchy; 
-	class Scene;
-	class Filter;
-}
-
+namespace Pg::Editor::Data { class DataContainer; }
 namespace Pg::Editor::Manager
 {
+	class WindowManager;
 	class EditorManager
 	{
 	public:
@@ -29,18 +20,8 @@ namespace Pg::Editor::Manager
 		void UIHandler(MSG message);
 
 	private:
-		Pg::UI::Manager::UIManager* _uiManager;
-		Pg::Editor::Helper::EditorHelper* _edHepler;
-
-		// Editor Windows
-		std::unique_ptr<Pg::Editor::Window::Inspector> _inspector;
-		std::unique_ptr<Pg::Editor::Window::Hierarchy> _hierarchy;
-		std::unique_ptr<Pg::Editor::Window::Scene> _scene;
-		std::unique_ptr<Pg::Editor::Window::Filter> _filter;
-
-		bool _editorOnOff;
-
-		std::string str;
+		Pg::Editor::Data::DataContainer* _dataContainer;
+		std::unique_ptr<Pg::Editor::Manager::WindowManager> _windowManager;
 	};
 }
 
