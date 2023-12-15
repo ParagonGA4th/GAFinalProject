ae97f173f5d1bd9ffac57eba188f63fa98dedbbb
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2023-12-15 15:42:14 +0900

    [고태욱] 셰이더 리팩토링 -> 파일 위치 이동 + 구조화. 결과적으로 Common을 구성해야 한다. (Unity 시각화 위해 세이브 커밋)

---
+++ ParagonEngine/Shaders/System/Libraries/System_1stLayouts.hlsli
@@ -0,0 +1,58 @@
+//그래픽 엔진 디폴트 InputLayout을 저장한 HLSL.
+
+#ifndef __DEFINED_SYSTEM_1STLAYOUTS_HLSL__
+#define __DEFINED_SYSTEM_1STLAYOUTS_HLSL__
+
+struct Vin1stStatic
+{
+    float3 vin1st_PosL : POSITION;
+    float3 vin1st_NormalL : NORMAL;
+    float3 vin1st_TangentL : TANGENT;
+    float4 vin1st_Color : COLOR;
+    float3 vin1st_Tex : TEXCOORD;
+    uint vin1st_MatID : MATERIALID;
+};
+
+struct Vin1stSkinned
+{
+    float3 vin1st_PosL : POSITION;
+    float3 vin1st_NormalL : NORMAL;
+    float3 vin1st_TangentL : TANGENT;
+    float4 vin1st_Color : COLOR;
+    float3 vin1st_Tex : TEXCOORD;
+    uint vin1st_MatID : MATERIALID;
+    
+    uint vin1st_BlendIndice0 : BLENDINDICES0;
+    uint vin1st_BlendIndice1 : BLENDINDICES1;
+    uint vin1st_BlendIndice2 : BLENDINDICES2;
+    uint vin1st_BlendIndice3 : BLENDINDICES3;
+    
+    float vin1st_BlendWeight0 : BLENDWEIGHT0;
+    float vin1st_BlendWeight1 : BLENDWEIGHT1;
+    float vin1st_BlendWeight2 : BLENDWEIGHT2;
+};
+
+struct VOut1st
+{
+    float4 vout1st_PosH : SV_POSITION;
+    float3 vout1st_PosW : POSITION;
+    float3 vout1st_NormalW : NORMAL;
+    float3 vout1st_TangentW : TANGENT;
+    float4 vout1st_Color : COLOR;
+    float3 vout1st_Tex : TEXCOORD;
+    uint vout1st_MatID : MATERIALID;
+};
+
+struct POut1st //RGBA
+{
+    float4 pout1st_RT0 : SV_Target0; // Texture UV Coords (RGB),  World Space Tangent.x (A)
+    float4 pout1st_RT1 : SV_Target1; // World Space Normal (RGB), World Space Tangent.y (A)
+    float4 pout1st_RT2 : SV_Target2; // World Space Position (RGB), World Space Tangent.z (A)
+    float4 pout1st_RT3 : SV_Target3; // 3D Model Color (RGB)
+    float4 pout1st_RT4 : SV_Target4; // Material ID ((uint)R), Screen Space Velocity (GB), Alpha (A)
+    float4 pout1st_RT5 : SV_Target5; // Temp Diffuse Texture;
+	float4 pout1st_RT6 : SV_Target6; // Phong Lighting Results
+    //float pout1st_Depth : SV_Depth; // Depth
+};
+
+#endif //__DEFINED_SYSTEM_1STLAYOUTS_HLSL__
---
+++ ParagonEngine/Shaders/System/Pipeline/FirstStage/FirstStage_PS.hlsl
@@ -0,0 +1,65 @@
+///그래픽엔진 자체의 1st Pixel Shader (All)
+
+#pragma pixel_shader main
+#pragma target 5.0
+
+#include "../../Libraries/System_PerObjectBuffers.hlsli"
+#include "../../Libraries/System_1stLayouts.hlsli"
+
+Texture2D Diffuse;
+Texture2D Normal;
+
+POut1st main(VOut1st input)
+{
+    POut1st output;
+	
+	// normal과 tangent는 rasterizer를 거치며 보간된다.
+	float3 NormalW = normalize(input.vout1st_NormalW);
+	float3 TangentW = normalize(input.vout1st_TangentW);
+	float3 BinormalW = normalize(cross(TangentW, NormalW));
+	float3x3 TBNMatrix = float3x3(TangentW, BinormalW, NormalW);
+	
+	float3 NormalSample = pow(Normal.Sample(state, input.vout1st_Tex.xy).xyz, 1.0 / 2.2); // [0 ~ 1]
+	NormalSample = NormalSample * 2.0 - 1.0;// [1 ~ -1]
+	
+	// convert to World Space
+	NormalSample = mul(NormalSample, TBNMatrix);
+	
+    //RT0 : Texture UV Coords.
+	output.pout1st_RT0.xyz = input.vout1st_Tex;
+    //RT0 : World Space Tangent.x
+	output.pout1st_RT0.w = input.vout1st_TangentW.x;
+    
+    //RT1 : World Space Normal.
+	//output.pout1st_RT1.xyz = input.vout1st_NormalW;
+	//output.pout1st_RT1.xyz = NormalW;
+	output.pout1st_RT1.xyz = NormalSample;
+    //RT1 : World Space Tangent.y
+	output.pout1st_RT1.w = input.vout1st_TangentW.y;
+    
+    //RT2 : World Space Position.
+	output.pout1st_RT2.xyz = input.vout1st_PosW;
+    //RT2 : World Space Tangent.z
+	output.pout1st_RT2.w = input.vout1st_TangentW.z;
+    
+    //RT3 : 3D Model Color.
+	output.pout1st_RT3 = input.vout1st_Color;
+    
+    //RT4 : Material ID. (위에서 일단 하드코딩 되어 0으로 내려옴)
+	output.pout1st_RT4.x = input.vout1st_MatID;
+    //RT4 : Screen Space Velocity. (0,0으로 하드코딩됨)
+	output.pout1st_RT4.yz = float2(0.0f, 0.0f);
+    //RT4 : Alpha. (1로 하드코딩됨)
+	output.pout1st_RT4.w = 1.0f;
+    
+    //RT5 : Temp Diffuse Texture
+    output.pout1st_RT5 = Diffuse.Sample(state, input.vout1st_Tex.xy);
+	
+	//RT6 : Phong Lighting Results
+  
+	//pout1st_Depth -> SV_Depth에 대해서는 아직은 추가적인 처리 안 해줄 것.
+    
+    
+    return output;
+}
+
