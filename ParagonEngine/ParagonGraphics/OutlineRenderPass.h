bf2e57820472808cbc28a03959ba13cc11589784
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2024-01-25 18:46:44 +0900

    [고태욱] Outline Shader 작업 중, Blur 이제 들어가야 한다. 방법은 CPP에 정리 : Stencil + Blur.

--- ParagonEngine/ParagonGraphics/OutlineRenderPass.h
+++ ParagonEngine/ParagonGraphics/OutlineRenderPass.h
@@ -33,6 +33,33 @@ namespace Pg::Graphics
 		virtual void ExecuteNextRenderRequirements() override;
 		virtual void PassNextRequirements(D3DCarrier& gCarrier) override;
 
+		void GetOutlinePlannedObjectID(unsigned int val);
+
+	private:
+		void BindObjectIndexConstantBuffer();
+		void BindVertexIndexBuffer();
+		void CreateObjectIndexConstantBuffer();
+
+	private:
+		//Carrier에서 임시로 보관하고 있는 SRV. ObjMat 제외 Clipping.
+		ID3D11ShaderResourceView* _objMatSaveSRV;
+
+		std::unique_ptr<GBufferRender> _outlineBufferRender;
+		std::unique_ptr<GBufferDepthStencil> _outlineMaskingGDS;
+
+		ID3D11DepthStencilState* _writeModeDSS = nullptr;
+
+		std::unique_ptr<SystemVertexShader> _vs;
+		std::unique_ptr<SystemPixelShader> _ps;
+
+	private:
+		//임시로 하이라이트할 오브젝트 아이디를 보관하는 변수.
+		unsigned int _toHighlightID = NULL;
+
+		ID3D11Buffer* _constantBufferObjID = nullptr;
+
+	private:
+		LowDX11Storage* _DXStorage;
 	};
 }
 
