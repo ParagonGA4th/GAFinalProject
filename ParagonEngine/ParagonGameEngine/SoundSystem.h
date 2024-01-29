1e7d399699b749a66051ede2e268c46361414b3f
Jisang Byeon <pannasonic74@gmail.com>
2024-01-29 17:50:00 +0900

    [변지상] fmod.dll 연동 성공

--- ParagonEngine/ParagonGameEngine/SoundSystem.h
+++ ParagonEngine/ParagonGameEngine/SoundSystem.h
@@ -1,6 +1,16 @@
 #pragma once
 #include "../ParagonProcess/CoreSingleton.h"
 
+//FMOD 라이브러리
+//#pragma comment(lib, "fmod_vc.lib")
+
+#include "../include/fmod/fmod.h"
+#include "../include/fmod/fmod.hpp"
+#include "../include/fmod/fmod_common.h"
+
+
+#include <vector>
+#include <string>
 /// <summary>
 /// 변지상의 SoundSystem
 /// FMOD를 기반으로 3D 사운드를 구현할 예정이다.
@@ -11,6 +21,16 @@ namespace Pg::Engine
 	{
 	public:
 		SoundSystem();
+
+		void Initialize();
+
+		void Finalize();
+
+		void CreateSound(std::string path);
+
+	private:
+		FMOD::System* _system;
+		FMOD::ChannelGroup* _channelGroup;  
 	};
 }
 
