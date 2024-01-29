c59bcf7767e229b184f0cbf5e644b2bdd3d8f1b1
Jisang Byeon <pannasonic74@gmail.com>
2024-01-29 10:02:15 +0900

    [변지상] SoundSystem 시작

--- ParagonEngine/ParagonGameEngine/SoundSystem.h
+++ ParagonEngine/ParagonGameEngine/SoundSystem.h
@@ -1,5 +1,16 @@
 #pragma once
-class SoundSystem
+#include "../ParagonProcess/CoreSingleton.h"
+
+/// <summary>
+/// 변지상의 SoundSystem
+/// FMOD를 기반으로 3D 사운드를 구현할 예정이다.
+/// </summary>
+namespace Pg::Engine
 {
-};
+	class SoundSystem
+	{
+	public:
+		SoundSystem();
+	};
+}
 
