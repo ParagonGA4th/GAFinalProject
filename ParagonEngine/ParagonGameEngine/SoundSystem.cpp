b162a6c2642dbf59d93d339aebdde1611719d1f9
Jisang Byeon <pannasonic74@gmail.com>
2024-01-29 17:50:00 +0900

    [변지상] fmod.dll 연동 성공

--- ParagonEngine/ParagonGameEngine/SoundSystem.cpp
+++ ParagonEngine/ParagonGameEngine/SoundSystem.cpp
@@ -2,5 +2,33 @@
 
 namespace Pg::Engine
 {
+	SoundSystem::SoundSystem() :
+		_system(),
+		_channelGroup()
+	{
+		//FMOD 초기화
+		FMOD::System_Create(&_system);
+
+		//시스템 초기화 (채널 설정을 동시에 해주어야 함)
+		_system->init(100, FMOD_INIT_NORMAL, NULL);
+
+		_system->set3DSettings(1.0f, 1.0f, 1.0f);
+
+	}
+
+	void SoundSystem::Initialize()
+	{
+
+	}
+
+	void SoundSystem::Finalize()
+	{
+
+	}
+
+	void SoundSystem::CreateSound(std::string path)
+	{
+
+	}
 
 }
