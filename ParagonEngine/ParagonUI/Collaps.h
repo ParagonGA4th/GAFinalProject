da5edf3506a52a59db4a6110124366dbb1242c54
Min-seo-Choi <mschoi0814@gmail.com>
2024-01-31 14:14:13 +0900

    [최민서] gizmo 및 remove component를 하기 위한 기초단계

--- ParagonEngine/ParagonUI/Collaps.h
+++ ParagonEngine/ParagonUI/Collaps.h
@@ -8,12 +8,13 @@ namespace Pg::UI::Widget::Layout
 	class Collaps : public Pg::UI::IWidget
 	{
 	public:
-		Collaps(std::string LayoutName, std::vector<Pg::UI::IWidget*> widgets);
+		Collaps(std::string LayoutName, std::vector<Pg::UI::IWidget*> widgets, bool* isShow = nullptr);
 		virtual void Update() override;
 
 	private:
 		std::string _layoutName;
 		std::vector < Pg::UI::IWidget*> _widgets;
+		bool* _isShow;
 	};
 }
 
