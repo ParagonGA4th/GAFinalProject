30ddae20dce67a350bfcb1abd8985067fd61cedb
Min-seo-Choi <mschoi0814@gmail.com>
2024-01-31 17:19:29 +0900

    [최민서] 아트에서 넘겨준 씬을 출력할 수 있도록 코드 수정

--- ParagonEngine/ParagonData/StaticMeshRenderer.cpp
+++ ParagonEngine/ParagonData/StaticMeshRenderer.cpp
@@ -1,6 +1,11 @@
 #include "StaticMeshRenderer.h"
 #include "../ParagonUtil/ResourceHelper.h"
 
+#include <generic_factory/generic_factory.hpp>
+
+using namespace Pg::Data;
+REGISTER_CHILD_INTO_FACTORY(RendererBase3D, StaticMeshRenderer, "class Pg::Data::StaticMeshRenderer", GameObject*);
+
 namespace Pg::Data
 {
 	StaticMeshRenderer::StaticMeshRenderer(GameObject* obj) : RendererBase3D(obj)
