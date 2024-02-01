bee3c1809cfbff055f8519b9622087785f4b80c8
Min-seo-Choi <mschoi0814@gmail.com>
2024-02-01 18:53:51 +0900

    [최민서] 카메라 값 수정 안되는 오류 수정

--- ParagonEngine/ParagonUI/InputFloat3.cpp
+++ ParagonEngine/ParagonUI/InputFloat3.cpp
@@ -2,26 +2,84 @@
 #include "imgui.h"
 
 Pg::UI::Widget::InputFloat3::InputFloat3(std::string label, Pg::Math::PGFLOAT3* input)
-	: _label("##"), _inputPGFloat(input)
+	: _label("##"), _inputIndex(0), _inputPGFloat3(input)
 {
 	_label.append(label);
 
-	_inputFloat[0] = _inputPGFloat->x;
-	_inputFloat[1] = _inputPGFloat->y;
-	_inputFloat[2] = _inputPGFloat->z;
+	_inputFloat[0] = _inputPGFloat3->x;
+	_inputFloat[1] = _inputPGFloat3->y;
+	_inputFloat[2] = _inputPGFloat3->z;
+} 
+
+Pg::UI::Widget::InputFloat3::InputFloat3(std::string label, Pg::Math::PGFLOAT4* input)
+	: _label("##"), _inputIndex(1), _inputPGFloat4(input)
+{
+	_label.append(label);
+
+	_inputFloat[0] = _inputPGFloat4->x;
+	_inputFloat[1] = _inputPGFloat4->y;
+	_inputFloat[2] = _inputPGFloat4->z;
+}
+
+Pg::UI::Widget::InputFloat3::InputFloat3(std::string label, Pg::Math::PGQuaternion* input)
+	: _label("##"), _inputIndex(2), _inputPGQuat(input)
+{
+	_label.append(label);
+
+	_inputPGFloat3 = new Pg::Math::PGFLOAT3;
+	*_inputPGFloat3 = Pg::Math::PGQuaternionToEuler(*_inputPGQuat);
+
+	_inputFloat[0] = _inputPGFloat3->x;
+	_inputFloat[1] = _inputPGFloat3->y;
+	_inputFloat[2] = _inputPGFloat3->z;
 } 
 
 void Pg::UI::Widget::InputFloat3::Update()
 {
-	_inputFloat[0] = _inputPGFloat->x;
-	_inputFloat[1] = _inputPGFloat->y;
-	_inputFloat[2] = _inputPGFloat->z;
+	if (_inputIndex == 0)
+	{
+		_inputFloat[0] = _inputPGFloat3->x;
+		_inputFloat[1] = _inputPGFloat3->y;
+		_inputFloat[2] = _inputPGFloat3->z;
+	}
+	else if (_inputIndex == 1)
+	{
+		_inputFloat[0] = _inputPGFloat4->x;
+		_inputFloat[1] = _inputPGFloat4->y;
+		_inputFloat[2] = _inputPGFloat4->z;
+	}
+	else
+	{
+		*_inputPGFloat3 = Pg::Math::PGQuaternionToEuler(*_inputPGQuat);
 
-	ImGui::InputFloat3(_label.c_str(), _inputFloat);
+		_inputFloat[0] = _inputPGFloat3->x;
+		_inputFloat[1] = _inputPGFloat3->y;
+		_inputFloat[2] = _inputPGFloat3->z;
+	}
 
-	_inputPGFloat->x = _inputFloat[0];
-	_inputPGFloat->y = _inputFloat[1];
-	_inputPGFloat->z = _inputFloat[2];
+	if (ImGui::InputFloat3(_label.c_str(), _inputFloat))
+	{
+		if (_inputIndex == 0)
+		{
+			_inputPGFloat3->x = _inputFloat[0];
+			_inputPGFloat3->y = _inputFloat[1];
+			_inputPGFloat3->z = _inputFloat[2];
+		}
+		else if (_inputIndex == 1)
+		{
+			_inputPGFloat4->x = _inputFloat[0];
+			_inputPGFloat4->y = _inputFloat[1];
+			_inputPGFloat4->z = _inputFloat[2];
+		}
+		else
+		{
+			_inputPGFloat3->x = _inputFloat[0];
+			_inputPGFloat3->y = _inputFloat[1];
+			_inputPGFloat3->z = _inputFloat[2];
+
+			*_inputPGQuat = Pg::Math::PGEulerToQuaternion(*_inputPGFloat3);
+		}
+	}
 }
 
 
