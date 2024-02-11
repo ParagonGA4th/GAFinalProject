5317ba97420f4b8bb3ff3706b420a8c9aeeb977f
jisnag <pannasonic74@gmail.com>
2024-02-11 21:10:52 +0900

    [변지상] SoundSystem 구조 확립 중...

--- ParagonEngine/ParagonGameEngine/SoundSystem.cpp
+++ ParagonEngine/ParagonGameEngine/SoundSystem.cpp
@@ -1,5 +1,6 @@
 #include "SoundSystem.h"
 #include "SceneSystem.h"
+#include "eSoundGroup.h"
 #include "../ParagonData/Scene.h"
 #include "../ParagonData/AudioSource.h"
 #include "../ParagonData/eSoundState.h"
@@ -19,6 +20,16 @@ namespace Pg::Engine
 		//채널 그룹 초기화
 		_system->getMasterChannelGroup(&_channelGroup);
 
+
+		// FMOD::ChannelGroup 초기화
+		for (int i = 0; i < (int)_maxGroup; i++)
+		{
+			// 개별 Group 초기화
+			_channelGroupVec[_eSoundGroup] = 0;
+			std::string temp = "GroupName" + std::to_string(i);
+			_system->createChannelGroup(temp.c_str(), &_channelGroupVec.at(_eSoundGroup));
+		}
+
 		//3D 사운드의 크기나 거리 등을 설정
 		//_system->set3DSettings(1.0f, 1.0f, 1.0f);
 
@@ -32,35 +43,45 @@ namespace Pg::Engine
 
 		_maxSound = _soundMap.size();
 
+		//test
+		CreateSound("../Resources/Sounds/Test/TitleBGM", eSoundGroup::BGM, false);
+		SetVolume("../Resources/Sounds/Test/TitleBGM", 1.0f);
 		
 	}
 
 
 	void SoundSystem::Update()
 	{
-		for (auto& obj : _sceneSystem->GetCurrentScene()->GetObjectList())
-		{
-			//if ()
-			//{
-			//	//CreateSound()
-			//}
-		}
+		//if (_audioSource->_soundState == Pg::Data::eSoundState::_PLAY)
+		//{
+		//	PlaySound("../Sounds/Test/TitleBGM");
+		//}
 		_system->update();
+
+		//test
+		PlaySound("../Resources/Sounds/Test/TitleBGM");
 	}
 
 	void SoundSystem::Finalize()
 	{
 		//채널 그룹 싹 다 해제.
-		for (int i = 0; i < _maxGroup; i++)
+		//for (int i = 0; i < _maxGroup; i++)
+		//{
+		//	//_channelGroupVec->release();
+		//	_channelGroupVec[i].clear();
+		//}
+
+		for (auto& iter : _channelGroupVec)
 		{
-			_channelGroupVec[i]->release();
+			iter.second->release();
 		}
 
+		_channelGroup->release();
 		_system->close();
 		_system->release();
 	}
 
-	void SoundSystem::CreateSound(std::string path, bool isLoop)
+	void SoundSystem::CreateSound(std::string path, eSoundGroup soundGroup, bool isLoop)
 	{
 		//사운드 객체 생성
 		Pg::Data::AudioData audioData;
@@ -74,10 +95,10 @@ namespace Pg::Engine
 			_system->createSound(path.c_str(), FMOD_LOOP_NORMAL | FMOD_DEFAULT, NULL, &(audioData.sound));
 		}
 
-		audioData.isPlaying = false;
-		audioData.channel = 0;
-		//audioData.channel->setChannelGroup(_channelGroupVec[_channelGroup]);
 		audioData.soundPath = path;
+		audioData.isPlaying = true;
+		audioData.channel = 0;
+		audioData.channel->setChannelGroup(_channelGroupVec[soundGroup]);
 
 		_soundMap.insert({ path, audioData });
 	}
@@ -86,10 +107,15 @@ namespace Pg::Engine
 	{
 		for (int i = 0; i < (int)_maxSound; i++)
 		{
+			//_system->playSound(_soundMap.begin(), NULL, false, &_soundMap.at(path).channel);
+			//_soundMap.at(path).isPlaying = true;
+
 			if (_soundMap.at(path).soundPath == path)
 			{
-				_system->playSound(_soundMap.at(path).sound, NULL, false, &_soundMap.at(path).channel);
-				_soundMap.at(path).isPlaying = true;
+				_system->playSound(_soundMap.at(path).sound, NULL, true, &_soundMap.at(path).channel);
+				_soundMap.at(path).channel->setPaused(false);
+				_soundMap.at(path).isPlaying = 
+					_soundMap.at(path).channel->getCurrentSound(&_soundMap.at(path).sound);;
 			}
 		}
 	}
