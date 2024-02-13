21f909d64dbd6bb1b459a6fe797166897fea6388
Min-seo-Choi <mschoi0814@gmail.com>
2024-02-13 17:25:40 +0900

    [최민서] 기즈모 출력 오류 수정 중

--- ParagonEngine/ParagonUI/Gizmo.cpp
+++ ParagonEngine/ParagonUI/Gizmo.cpp
@@ -3,11 +3,11 @@
 #include "imgui.h"
 #include "ImGuizmo.h"
 
-#include "../ParagonData/Camera.h"
-#include "../ParagonData/Transform.h"
+
 
 Pg::UI::Helper::Gizmo::Gizmo()
 {
+
 }
 
 void Pg::UI::Helper::Gizmo::CreateFrame()
@@ -18,40 +18,32 @@ void Pg::UI::Helper::Gizmo::CreateFrame()
 
 void Pg::UI::Helper::Gizmo::SetCamera(Pg::Data::Camera* camera)
 {
+	_camera = camera;
+
 	//proj
-	_cameraProj[0][0] = camera->GetProjMatrix()._11; _cameraProj[0][1] = camera->GetProjMatrix()._12; 
-	_cameraProj[0][2] = camera->GetProjMatrix()._13; _cameraProj[0][3] = camera->GetProjMatrix()._14;
-	_cameraProj[1][0] = camera->GetProjMatrix()._21; _cameraProj[1][1] = camera->GetProjMatrix()._22; 
-	_cameraProj[1][2] = camera->GetProjMatrix()._23; _cameraProj[1][3] = camera->GetProjMatrix()._24;
-	_cameraProj[2][0] = camera->GetProjMatrix()._31; _cameraProj[2][1] = camera->GetProjMatrix()._32; 
-	_cameraProj[2][2] = -camera->GetProjMatrix()._33; _cameraProj[2][3] = -camera->GetProjMatrix()._34;
-	_cameraProj[3][0] = camera->GetProjMatrix()._41; _cameraProj[3][1] = camera->GetProjMatrix()._42; 
-	_cameraProj[3][2] = camera->GetProjMatrix()._43; _cameraProj[3][3] = camera->GetProjMatrix()._44;
+	_ptm = ConvertPTM(camera->GetProjMatrix());	
+	//_ptm = glm::perspective(glm::radians(camera->_fovY), 16.0f / 9.0f, camera->_nearZ, camera->_farZ);
+
 
 	// view
-	//DirectX::XMMATRIX wtm = *reinterpret_cast<DirectX::XMMATRIX*>(const_cast<Pg::Math::PGFLOAT4X4*>(&camera->GetViewMatrix()));
-	//DirectX::XMVECTOR d;
-	//auto im = *reinterpret_cast<Pg::Math::PGFLOAT4X4*>(&DirectX::XMMatrixInverse(&d, wtm));
+	_vtm = ConvertVTM(camera->GetViewMatrix());
+	//DirectX::XMMATRIX viewMT = Pg::Math::PG2XM_MATRIX4X4(camera->GetViewMatrix());
 
+	//_vtm[2][0] = -DirectX::XMVectorGetX(viewMT.r[2]);
+	//_vtm[2][1] = -DirectX::XMVectorGetY(viewMT.r[2]);
+	//_vtm[2][2] = -DirectX::XMVectorGetZ(viewMT.r[2]);
+	//_vtm[2][3] = -DirectX::XMVectorGetW(viewMT.r[2]);
+
+	//// 나머지 행들 복사
+	//_vtm[0] = glm::make_vec4(&viewMT.r[0].m128_f32[0]);
+	//_vtm[1] = glm::make_vec4(&viewMT.r[1].m128_f32[0]);
+	//_vtm[3] = glm::make_vec4(&viewMT.r[3].m128_f32[0]);
 }
 
 void Pg::UI::Helper::Gizmo::SetTransform(Pg::Data::Transform* trans)
 {
-	//auto ftm = *reinterpret_cast<DirectX::XMcamera->GetProjMatrix()RIX*>(const_cast<yunuGI::camera->GetProjMatrix()rix4x4*>(&camera->GetProjMatrix()));
-	//DirectX::XMVECTOR scale;
-	//DirectX::XMVECTOR rotation;
-	//DirectX::XMVECTOR position;
-
-	//DirectX::XMcamera->GetProjMatrix()rixDecompose(&scale, &rotation, &position, ftm);
-
-	//yunuGI::Vector4 fs = *reinterpret_cast<yunuGI::Vector4*>(&scale);
-	//yunuGI::Vector4 fr = *reinterpret_cast<yunuGI::Vector4*>(&rotation);
-	//yunuGI::Vector4 fp = *reinterpret_cast<yunuGI::Vector4*>(&position);
-
-	//fr.x *= -1;
-	//fr.y *= -1;
-	//auto finalcamera->GetProjMatrix() = glm::translate(glm::camera->GetProjMatrix()4(1.0f), glm::vec3(fp.x, fp.y, -fp.z)) * glm::camera->GetProjMatrix()4_cast(*reinterpret_cast<glm::quat*>(&fr)) * glm::scale(glm::camera->GetProjMatrix()4(1.0f), glm::vec3(fs.x, fs.y, fs.z));
-
+	_trans = trans;
+	_wtm = ConvertWTM(trans->GetWorldTM());
 }
 
 void Pg::UI::Helper::Gizmo::SetWindowSize(float width, float height)
@@ -60,30 +52,135 @@ void Pg::UI::Helper::Gizmo::SetWindowSize(float width, float height)
 	_displayHeight = height;
 }
 
-void Pg::UI::Helper::Gizmo::ConvertPTM(Pg::Math::PGFLOAT4X4& mt)
+glm::mat4 Pg::UI::Helper::Gizmo::ConvertPTM(Pg::Math::PGFLOAT4X4 mt)
+{
+	glm::mat4 finalTM;
+
+	finalTM[0][0] = mt._11; finalTM[0][1] = mt._12; finalTM[0][2] = mt._13; finalTM[0][3] = mt._14;
+	finalTM[1][0] = mt._21; finalTM[1][1] = mt._22; finalTM[1][2] = mt._23; finalTM[1][3] = mt._24;
+	finalTM[2][0] = mt._31; finalTM[2][1] = mt._32; finalTM[2][2] = -mt._33; finalTM[2][3] = -mt._34;
+	finalTM[3][0] = mt._41; finalTM[3][1] = mt._42; finalTM[3][2] = mt._43; finalTM[3][3] = mt._44;
+
+	return finalTM;
+}
+
+void Pg::UI::Helper::Gizmo::ConvertPTM(glm::mat4 mt)
 {
 
 }
 
-void Pg::UI::Helper::Gizmo::ConvertVTM(Pg::Math::PGFLOAT4X4& mt)
+glm::mat4 Pg::UI::Helper::Gizmo::ConvertVTM(Pg::Math::PGFLOAT4X4 mt)
+{
+	//auto ftm = Pg::Math::PG2XM_MATRIX4X4(mt);
+
+	//DirectX::XMVECTOR scale;
+	//DirectX::XMVECTOR rotation;
+	//DirectX::XMVECTOR position;
+
+	//DirectX::XMMatrixDecompose(&scale, &rotation, &position, ftm);
+
+
+
+	return glm::inverse(ConvertWTM(mt.Inverse()));
+}
+
+void Pg::UI::Helper::Gizmo::ConvertVTM(glm::mat4 mt)
 {
 
 }
 
-void Pg::UI::Helper::Gizmo::ConvertWTM(Pg::Math::PGFLOAT4X4& mt)
+glm::mat4 Pg::UI::Helper::Gizmo::ConvertWTM(Pg::Math::PGFLOAT4X4 mt)
 {
+	auto ftm = Pg::Math::PG2XM_MATRIX4X4(mt);
 
+	DirectX::XMVECTOR scale;
+	DirectX::XMVECTOR rotation;
+	DirectX::XMVECTOR position;
+
+	DirectX::XMMatrixDecompose(&scale, &rotation, &position, ftm);
+
+	Pg::Math::PGFLOAT4 fs = Pg::Math::XM2PG_FLOAT4_VECTOR(scale);
+	Pg::Math::PGFLOAT4 fr = Pg::Math::XM2PG_FLOAT4_VECTOR(rotation);
+	Pg::Math::PGFLOAT4 fp = Pg::Math::XM2PG_FLOAT4_VECTOR(position);
+
+
+	Pg::Math::PGQuaternion tFRQuat;
+	tFRQuat.w = fr.w;
+	tFRQuat.x = fr.x;
+	tFRQuat.y = fr.y;
+	tFRQuat.z = fr.z;
+
+
+	Pg::Math::PGFLOAT3 tEulerRot = Pg::Math::PGQuaternionToEuler(tFRQuat);
+	tEulerRot.x *= -1.f;
+	tEulerRot.y *= -1.f;
+
+	tFRQuat = Pg::Math::PGEulerToQuaternion(tEulerRot);
+	fr.w = tFRQuat.w;
+	fr.x = tFRQuat.x;
+	fr.y = tFRQuat.y;
+	fr.z = tFRQuat.z;
+
+	//fr.x *= -1;
+	//fr.y *= -1;
+
+	auto finalMat = 
+		glm::translate(glm::mat4(1.0f), 
+		glm::vec3(fp.x, fp.y, -fp.z)) * glm::mat4_cast(*reinterpret_cast<glm::quat*>(&fr)) * glm::scale(glm::mat4(1.0f), 
+		glm::vec3(fs.x, fs.y, fs.z));
+
+	return finalMat;
+}
+
+void Pg::UI::Helper::Gizmo::ConvertWTM(glm::mat4 mt)
+{
+	glm::vec3 scale;
+	glm::quat rotation;
+	glm::vec3 position;
+	glm::vec3 skew;
+	glm::vec4 perspective;
+	glm::decompose(mt, scale, rotation, position, skew, perspective);
+
+	_trans->_position = { position.x, position.y, -position.z };
+	_trans->_rotation = { -rotation.x, -rotation.y, rotation.z, rotation.w };
+	_trans->_scale = { scale.x, scale.y, scale.z };
 }
 
 void Pg::UI::Helper::Gizmo::DrawGizmo()
 {
-	ImGuizmo::SetRect(0, 0, _displayWidth, _displayHeight);
+	if (_trans != nullptr)
+	{
+		//auto ftm = Pg::Math::PG2XM_MATRIX4X4(_trans->GetWorldTM());
 
-	//ImGuizmo::Manipulate(
-	//	&(_cameraView[0][0]),
-	//	&(_cameraProj[0][0]),
-	//	ImGuizmo::TRANSLATE,
-	//	ImGuizmo::LOCAL,
-	//	&(_transcamera->GetProjMatrix()rix[0][0])
-	//);
+		//DirectX::XMVECTOR scale;
+		//DirectX::XMVECTOR rotation;
+		//DirectX::XMVECTOR position;
+
+		//DirectX::XMMatrixDecompose(&scale, &rotation, &position, ftm);
+
+		//Pg::Math::PGFLOAT4 fs = Pg::Math::XM2PG_FLOAT4_VECTOR(scale);
+		//Pg::Math::PGFLOAT4 fr = Pg::Math::XM2PG_FLOAT4_VECTOR(rotation);
+		//Pg::Math::PGFLOAT4 fp = Pg::Math::XM2PG_FLOAT4_VECTOR(position);
+
+
+		////auto t = DirectX::XMMatrixTranslation(fp.x, fp.y, -fp.z);
+		////t = DirectX::XMMatrixTranspose(t);
+		////DirectX::XMFLOAT4X4 tVal;
+		////DirectX::XMStoreFloat4x4(&tVal, t);
+		//glm::vec3 trans_vec(fp.x, fp.y, -fp.z);
+		//glm::mat4 Model = glm::mat4(1.0f); // 모델 좌표계 생성
+		//_wtm = glm::translate(Model, trans_vec);
+
+		ImGuizmo::SetRect(0, 0, _displayWidth, _displayHeight);
+
+		ImGuizmo::Manipulate(
+			&(_vtm[0][0]),		// cameraView
+			&(_ptm[0][0]),		// cameraProj
+			ImGuizmo::TRANSLATE,
+			ImGuizmo::LOCAL,
+			&(_wtm[0][0])		// object Transform
+		);
+
+		ConvertWTM(_wtm);
+	}
 }
