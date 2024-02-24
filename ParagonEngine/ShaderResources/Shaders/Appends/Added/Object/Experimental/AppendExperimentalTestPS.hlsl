e1fad6d9bd4e63cb95d441aea43598d33941c64c
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2024-02-24 21:02:59 +0900

    [고태욱] LinearDepth 테스팅 중.

--- ParagonEngine/ShaderResources/Shaders/Appends/Added/Object/Experimental/AppendExperimentalTestPS.hlsl
+++ ParagonEngine/ShaderResources/Shaders/Appends/Added/Object/Experimental/AppendExperimentalTestPS.hlsl
@@ -13,24 +13,60 @@ POutQuad main(VOutQuad pin)
     POutQuad res;
     
     float2 tActualUV = GetUV_F2(pin.UV);
-    float tVal = GetDepth(tActualUV);
+    float depthValue = GetDepth(tActualUV);
+    
+     // 처음 10% 범위는 빨간색
+    if (depthValue < 0.9f)
+    {
+        res.Output = float4(1.0, 0.0f, 0.0f, 1.0f);
+    }
+    
+    // 다음 0.025% 범위는 녹색
+    if (depthValue > 0.9f)
+    {
+        res.Output = float4(0.0, 1.0f, 0.0f, 1.0f);
+    }
+
+    // 나머지 깊이 버퍼는 파란색
+    if (depthValue > 0.925f)
+    {
+        res.Output = float4(0.0, 0.0f, 1.0f, 1.0f);
+    }
+    
+    //더 깊으면?
+    if (depthValue > 0.95f)
+    {
+        res.Output = float4(1.0f, 0.0f, 1.0f, 1.0f);
+    }
+    
+    if (depthValue > 0.99f)
+    {
+        res.Output = float4(1.0f, 1.0f, 0.0f, 1.0f);
+    }
+    
+    if (depthValue > 0.995f)
+    {
+        res.Output = float4(0.0f, 1.0f, 1.0f, 1.0f);
+    }
+    
+    //더 깊이 보기.
     
     //0이랑 가까울수록 카메라 근접,
     //1 가까울수록 카메라 멀리.
-    float fogRatio = 0.5f;
-    if (tVal > fogRatio)
-    {
-        //영향을 받는 범위.
-        //실제로 White로 표시.
-        res.Output = float4(tVal, tVal, tVal, 1.0f);
-        
-        
-    }
-    else
-    {
-        //영향을 안 받는 범위.
-        res.Output = float4(0.0f, 1.0f, 0.0f, 1.0f);
-    }
+   //float fogRatio = 0.5f;
+   //if (tVal > fogRatio)
+   //{
+   //    //영향을 받는 범위.
+   //    //실제로 White로 표시.
+   //    res.Output = float4(tVal, tVal, tVal, 1.0f);
+   //    
+   //    
+   //}
+   //else
+   //{
+   //    //영향을 안 받는 범위.
+   //    res.Output = float4(0.0f, 1.0f, 0.0f, 1.0f);
+   //}
 
     return res;
 }
