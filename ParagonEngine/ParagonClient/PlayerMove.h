cac4ef96a4a83489d0e70624d888a3da32c25c4a
Jisang Byeon <pannasonic74@gmail.com>
2024-03-05 20:05:24 +0900

    [변지상] 엔진에 존재하던 Script들 전부 Client로 이동시킴

--- ParagonEngine/ParagonClient/PlayerMove.h
+++ ParagonEngine/ParagonClient/PlayerMove.h
@@ -1,38 +1,35 @@
-//#pragma once
-//#include "../ParagonData/Script.h"
-//#include "../ParagonData/Camera.h"
-//
-///// <summary>
-///// 플레이어 관련 스크립트
-///// </summary>
-//
-//namespace Pg::Data
-//{
-//	class GameObject;
-//	class DynamicCollider;
-//}
-//
-//namespace Pg::Engine
-//{
-//	namespace Input
-//	{
-//		class InputSystem;
-//	}
-//}
-//
-//class PlayerMove : public Pg::Data::Script
-//{
-//public:
-//	PlayerMove(Pg::Data::GameObject* obj);
-//
-//public:
-//	virtual void Start() override;
-//	virtual void Update() override;
-//
-//private:
-//	// 인풋 시스템으로 카메라의 이동을 체크한다
-//	Pg::Engine::Input::InputSystem* tInput = nullptr;
-//
-//	Pg::Data::DynamicCollider* dynamicCol;
-//};
-//
+#pragma once
+#include "../ParagonData/Script.h"
+
+/// <summary>
+/// 플레이어 관련 스크립트
+/// </summary>
+
+namespace Pg::Data
+{
+	class GameObject;
+	class DynamicCollider;
+}
+
+namespace Pg::API
+{
+	namespace Input
+	{
+		class PgInput;
+	}
+}
+
+class PlayerMove : public Pg::Data::Script
+{
+public:
+	PlayerMove(Pg::Data::GameObject* obj);
+
+public:
+	virtual void Start() override;
+	virtual void Update() override;
+
+private:
+	Pg::Data::DynamicCollider* dynamicCol;
+	Pg::API::Input::PgInput* _pgInput = nullptr;
+};
+
