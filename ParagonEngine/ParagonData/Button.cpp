65665f49ca6a254e10564e92496d3505c1c70d28
Jisang Byeon <pannasonic74@gmail.com>
2024-03-08 19:34:29 +0900

    [변지상] Slider 및 Handle 제작 중

--- ParagonEngine/ParagonData/Button.cpp
+++ ParagonEngine/ParagonData/Button.cpp
@@ -2,6 +2,7 @@
 #include "ImageRenderer.h"
 #include "TextRenderer.h"
 #include "../ParagonUtil/Log.h"
+#include "../ParagonUtil/InputSystem.h"
 
 #include "GameObject.h"
 
@@ -9,19 +10,22 @@ namespace Pg::Data
 {
 	class GameObject;
 
-	Button::Button(GameObject* obj)
-		:Component(obj),
+	Button::Button(GameObject* owner)
+		:Component(owner),
 		_onClickEvent(),
 		_isPressed(false)
 	{
-		
-		if (obj->GetComponent<ImageRenderer>())
+		//input 적용
+		auto& _tInputSystem = singleton<Pg::Util::Input::InputSystem>();
+		_inputSystem = &_tInputSystem;
+
+		if (owner->GetComponent<ImageRenderer>())
 		{
 			assert(false);
 		}
 		
 		
-		_imageRenderer = obj->AddComponent<ImageRenderer>();
+		_imageRenderer = owner->AddComponent<ImageRenderer>();
 		_imageWidth = &(_imageRenderer->_width);
 		_imageHeight = &(_imageRenderer->_height);
 		
@@ -35,6 +39,32 @@ namespace Pg::Data
 		_box2dInfo.LT = { worldPos.x - (*_imageWidth / 2.0f), worldPos.y - (*_imageHeight / 2.f) };
 		_box2dInfo.RB = { worldPos.x + (*_imageWidth / 2.0f), worldPos.y + (*_imageHeight / 2.f) };
 		_box2dInfo.color = { 1.0f,0.0f,0.0f,1.0f };
+
+		// gainput을 사용하여 비율 변환한 Click 처리
+		const float mouseX = _inputSystem->GetMouseX();
+		const float mouseY = _inputSystem->GetMouseY();
+
+		//화면 크기 고정이기에 수치 때려넣음.
+		const float screenWidth = 1920;
+		const float screenHeight = 1080;
+
+		const float objectX = _object->_transform._position.x;
+		const float objectY = _object->_transform._position.y;
+
+		// 화면 비율에 따라 마우스 위치 변환
+		const float scaledMouseX = mouseX * screenWidth;
+		const float scaledMouseY = mouseY * screenHeight;
+
+		if (scaledMouseX > (objectX - GetImageWidth() / 2.0f) &&
+			scaledMouseX <  (objectX + GetImageWidth() / 2.0f) &&
+			scaledMouseY >(objectY - GetImageHeight() / 2.0f) &&
+			scaledMouseY < (objectY + GetImageHeight() / 2.0f))
+		{
+			if (_inputSystem->GetKeyDown(API::Input::MouseLeft))
+			{
+				Click();
+			}
+		}
 	}
 
 	void Button::SetOnClickEvent(std::function<void()> onClickEvent)
@@ -56,7 +86,7 @@ namespace Pg::Data
 
 		_isPressed = !_isPressed;
 
-		//PG_TRACE("Button Click!");
+		PG_TRACE("Button Click!");
 	}
 
 	void Button::SetImagePath(const std::string& path)
