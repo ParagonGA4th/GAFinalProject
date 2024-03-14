3f4a50543339b712accd1399bde107f80e67b286
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2024-03-14 16:45:12 +0900

    [고태욱] 돌입 전, 코드 안정화

--- ParagonEngine/ParagonGraphics/SceneInformationSender.cpp
+++ ParagonEngine/ParagonGraphics/SceneInformationSender.cpp
@@ -1,10 +1,22 @@
 #include "SceneInformationSender.h"
+#include "GBufferRender.h"
+#include "GBufferDepthStencil.h"
+#include "LowDX11Storage.h"
+#include "LayoutDefine.h"
+#include "SystemVertexShader.h"
+#include "RenderTexture2D.h"
+#include "RenderCubemap.h"
+#include "SystemPixelShader.h"
+#include "GraphicsResourceManager.h"
+#include "../ParagonData/ParagonDefines.h"
+
+#include <algorithm> 
 
 namespace Pg::Graphics
 {
 	SceneInformationSender::SceneInformationSender()
 	{
-
+		_DXStorage = LowDX11Storage::GetInstance();
 	}
 
 	SceneInformationSender::~SceneInformationSender()
