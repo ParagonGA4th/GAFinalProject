7c9c26c980efe6483af8b0410741914f76a33736
Jisang Byeon <pannasonic74@gmail.com>
2024-03-14 19:46:00 +0900

    [변지상] Recast 구조 생각 완료, PlaneNavMesh 제작

---
+++ ParagonEngine/ParagonData/NavigationField.cpp
@@ -0,0 +1,23 @@
+#include "NavigationField.h"
+#include "GameObject.h"
+
+namespace Pg::Data
+{
+
+	NavigationField::NavigationField(GameObject* owner) :
+		Component(owner)
+	{
+
+	}
+
+	void NavigationField::Start()
+	{
+
+	}
+
+	void NavigationField::Update()
+	{
+
+	}
+
+}
