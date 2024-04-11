cb4bb289e83ae1cb5672d6b48f6c0563d9fc5431
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2024-04-11 16:45:25 +0900

    [고태욱] 언리얼 엔진 기본 Tonemapping ACES Filmic Tonemapping 적용. 어느정도 컬러 맞춤!

--- ParagonEngine/ShaderResources/Shaders/System/Pipeline/FinalStage/FinalStage_PS.hlsl
+++ ParagonEngine/ShaderResources/Shaders/System/Pipeline/FinalStage/FinalStage_PS.hlsl
@@ -1,6 +1,7 @@
 //SystemQuad에서 MainRenderTarget으로 뿌려주는 역할. (PS)
 #include "../../Libraries/System_QuadLayouts.hlsli"
 #include "../../../Appends/Libraries/SamplerStates/Appends_SamplerStates.hlsli"
+#include "../../../Appends/Libraries/MathFunctions/Appends_MathFunctions.hlsli"
 
 Texture2D<float4> QuadRTV : register(t5);
 
@@ -9,7 +10,18 @@ POutQuad main(VOutQuad pin)
 {
     POutQuad res;
     //자체를 MainRenderTarget에게 전달할 의도밖에 없다. UV 변환을 두번 거칠 필요가 없다.
-    res.Output = QuadRTV.Sample(fullScreenQuadSS, pin.UV);
+    float3 x = QuadRTV.Sample(fullScreenQuadSS, pin.UV).xyz;
+    
+    //<ACES Filmic Tone Mapping Curve>
+    float a = 2.51f;
+    float b = 0.03f;
+    float c = 2.43f;
+    float d = 0.59f;
+    float e = 0.14f;
+    
+    float3 val = gammaCorrection(saturate((x * (a * x + b)) / (x * (c * x + d) + e)));
+    //</ACES Filmic Tone Mapping Curve>
+    res.Output = float4(val, 1.0f);
     
     return res;
 }
