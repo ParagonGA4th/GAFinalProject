493cdb6537f4eaf199299ad9ab9c32c3ab23f6c0
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2024-04-16 10:36:49 +0900

    [고태욱] 브랜치 합칠용 임시 커밋, 인게임 로직 작업중

---
+++ ParagonEngine/ParagonScript/CombatSystem.cpp
@@ -0,0 +1,20 @@
+#include "CombatSystem.h"
+
+namespace Pg::DataScript
+{
+	CombatSystem::CombatSystem(Pg::Data::GameObject* obj) : ScriptInterface(obj)
+	{
+
+	}
+
+	void CombatSystem::Start()
+	{
+
+	}
+
+	void CombatSystem::Update()
+	{
+
+	}
+
+}
