113b3ab8c797f6e8a6411e2b56e7c864677532b0
Min-seo-Choi <mschoi0814@gmail.com>
2024-04-18 13:31:10 +0900

    [최민서] 콤보시스템 구현(테스트 필요)

--- ParagonEngine/ParagonScript/ComboSystem.cpp
+++ ParagonEngine/ParagonScript/ComboSystem.cpp
@@ -1,10 +1,12 @@
 #include "ComboSystem.h"
 #include "../ParagonAPI/PgTime.h"
+
+#include <math.h>
 #include <singleton-cpp/singleton.h>
 
 Pg::DataScript::ComboSystem::ComboSystem(Pg::Data::GameObject* obj)
 	: ScriptInterface(obj),
-	_prevCombo(0, 0.f), _comboCount(0), _time(0)
+	_prevCombo(0, 0.f), _comboCount(0), _time(0.f), _delay(0.3f)
 {
 	auto& tdelta = singleton<Pg::API::Time::PgTime>();
 	_deltaTime = &tdelta;
@@ -31,41 +33,31 @@ void Pg::DataScript::ComboSystem::ReSetCombo()
 
 void Pg::DataScript::ComboSystem::HitObject(bool isHit)
 {
-	_comboCount++;
-//
-//			if (counting % 3 == 0)
-//			{
-//				if (prevComboData.first == 0)
-//				{
-//					attack = comboRoutine.at(1);
-//					prevComboData.first = 1;
-//				}
-//				else
-//				{
-//					attack = comboRoutine.at(3);
-//					prevComboData.first = 3;
-//				}
-//			}
-//			else
-//			{
-//				if (prevComboData.first == 0)
-//				{
-//					attack = comboRoutine.at(1);
-//					prevComboData.first = 1;
-//				}
-//
-//				else if (prevComboData.first == 1)
-//				{
-//					attack = comboRoutine.at(2);
-//					prevComboData.first = 2;
-//				}
-//
-//				else if (prevComboData.first == 3)
-//				{
-//					attack = comboRoutine.at(1);
-//					prevComboData.first = 1;
-//				}
-//			}
+	if (!isHit) ReSetCombo(); // enemy가 아닌 오브젝트를 Hit 했을 경우 모든 콤보 리셋
+	else
+	{
+		// 콤보 딜레이
+		if (std::abs(_prevCombo.second - _time) <= _delay)
+		{
+			// 딜레이 보다 짧은 시간 내에 공격에 성공 한다면 combo++
+			_comboCount == 3 ? 1 : _comboCount++;	
+
+			// 성공한 콤보 저장
+			_prevCombo.first = _comboCount;
+			_prevCombo.second = _time;
+		}
+		else
+		{
+			// 딜레이 보다 늦은 시간 내에 공격 한다면 처음 부터 다시 시작
+			_comboCount = 1;
+
+			_prevCombo.first = _comboCount;
+			_prevCombo.second = _time;
+		}
+		
+		// 타임 리셋
+		_time = 0.f;
+	}
 }
 
 int Pg::DataScript::ComboSystem::GetComboCount()
