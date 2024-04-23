65886a9c9cc6ecd4ca33fbf9d0595d8d5ef5ba42
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2024-04-23 12:32:19 +0900

    [고태욱] Event 다수 추가 + Player 로직 일부 넣었음. IScriptResettable 역시 추가 (리로드시 객체 삭제 없이 데이터 리셋할 수 있게)

--- ParagonEngine/ParagonScript/PlayerBattleBehavior.h
+++ ParagonEngine/ParagonScript/PlayerBattleBehavior.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "ScriptInterface.h"
 #include "IObserver.h"
+#include "IScriptResettable.h"
 
 namespace Pg::DataScript
 {
@@ -9,9 +10,17 @@ namespace Pg::DataScript
 
 namespace Pg::DataScript
 {
-	class PlayerBattleBehavior : public ScriptInterface<PlayerBattleBehavior>, public IObserver
+	class PlayerBattleBehavior : public ScriptInterface<PlayerBattleBehavior>, public IObserver, public IScriptResettable
 	{
 		DEFINE_PARAGON_SCRIPT(PlayerBattleBehavior);
+
+	public:
+		//디파인.
+		inline static const float MAX_PLAYER_HEALTH = 100.0f;
+		inline static const float PLAYER_HEALTH_LOW_BARRIER = 20.0f;
+		inline static const float MAX_PLAYER_MANA = 100.0f;
+		inline static const float MAX_PLAYER_STAMINA = 100.0f;
+
 	public:
 		PlayerBattleBehavior(Pg::Data::GameObject* obj); 
 
@@ -20,13 +29,24 @@ namespace Pg::DataScript
 		virtual void Update();
 		virtual void LateUpdate();
 
-		//IObserver
+		//IObserver : 전체적인 Event 전달을 기준으로 작동할 것.
 		virtual void HandleEvents(const IEvent& e, UsedVariant usedVar1, UsedVariant usedVar2) override;
 	
+		//IScriptResettable. 다시 자기 자신을 리셋하는 함수.
+		virtual void ResetAll() override;
+
+
+		//플레이어에게 들어오는 개별적인 로직은 따로 분리됨.
+		void ChangePlayerHealth(float level);
+		void ChangePlayerMana(float level);
+		void ChangePlayerStamina(float level);
+
 	public:
 		//체력 관련. -> 지금은 맵 기믹때문에 이렇게 해놨지만, 나중에는 별도로 이동해야.
-		float healthPoint{ 100.0f };
-
+		//최고 체력은 일단 100이다.
+		float healthPoint{ MAX_PLAYER_HEALTH };
+		float manaPoint{ MAX_PLAYER_MANA };
+		float staminaPoint{ MAX_PLAYER_STAMINA };
 
 	private:
 		CombatSystem* _combatSystem{ nullptr };
