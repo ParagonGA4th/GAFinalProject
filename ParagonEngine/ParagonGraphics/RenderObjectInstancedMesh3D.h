7347dc78a3c4969ed87de6be43a1813014363f88
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2024-05-02 16:59:19 +0900

    [고태욱] 일단은 링크오류 없이 빌드되게 커밋.

--- ParagonEngine/ParagonGraphics/RenderObjectInstancedMesh3D.h
+++ ParagonEngine/ParagonGraphics/RenderObjectInstancedMesh3D.h
@@ -13,6 +13,19 @@ namespace Pg::Graphics
 		RenderObjectInstancedMesh3D(Pg::Data::BaseRenderer* baseRenderer, unsigned int objID);
 		~RenderObjectInstancedMesh3D();
 
+		virtual void CreateObjMatBuffers() override;
+	public:
+		//SkinnedMesh에 예외적으로 활용됨. 프레임을 진행시키기 위해서, 애니메이션 로직을 딱 한번, 미리 판단한다.
+		virtual void First_UpdateConstantBuffers(Pg::Data::CameraData* camData) override;
+		virtual void First_BindBuffers() override;
+		virtual void First_Render(const float* const dt) override;
+		virtual void First_UnbindBuffers() override;
+
+		virtual void ObjMat_UpdateConstantBuffers(Pg::Data::CameraData* camData) override;
+		virtual void ObjMat_BindBuffers() override;
+		virtual void ObjMat_Render(const float* const dt) override;
+		virtual void ObjMat_UnbindBuffers() override;
+
 	};
 }
 
