4a70fdd20b6cf81c309492bee2690fb8e65d90cf
Min-seo-Choi <mschoi0814@gmail.com>
2024-05-03 17:01:25 +0900

    [최민서][고태욱] gamemanager를 만들기 위한 1차 빌드업

---
+++ ParagonEngine/ParagonScript/GameManager.cpp
@@ -0,0 +1,16 @@
+#include "GameManager.h"
+
+void Pg::DataScript::GameManager::Awake()
+{
+
+}
+
+void Pg::DataScript::GameManager::Start()
+{
+
+}
+
+void Pg::DataScript::GameManager::Update()
+{
+
+}
