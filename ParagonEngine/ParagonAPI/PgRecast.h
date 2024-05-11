c7b534f519d9b81af998923638795ae64d95d959
jisnag <pannasonic74@gmail.com>
2024-05-11 17:33:37 +0900

    [변지상] API 경로 문제 빌드 되게 해놓음.

--- ParagonEngine/ParagonAPI/PgRecast.h
+++ ParagonEngine/ParagonAPI/PgRecast.h
@@ -2,22 +2,22 @@
 #include "../ParagonMath/PgMath.h"
 
 
-namespace Pg::Engine
-{
-	class Navigation;
-}
-
-namespace Pg::API::Recast
-{
-	class PgRecast
-	{
-	public:
-		void Initialize();
-
-		void SetSEpos(int index, Pg::Math::PGFLOAT3 startPosition, Pg::Math::PGFLOAT3 endPosition);
-		
-	private:
-		Pg::Engine::Navigation* _navSystem;
-	};
-}
+//namespace Pg::Engine
+//{
+//	class Navigation;
+//}
+//
+//namespace Pg::API::Recast
+//{
+//	class PgRecast
+//	{
+//	public:
+//		void Initialize();
+//
+//		void SetSEpos(int index, Pg::Math::PGFLOAT3 startPosition, Pg::Math::PGFLOAT3 endPosition);
+//		
+//	private:
+//		Pg::Engine::Navigation* _navSystem;
+//	};
+//}
 
