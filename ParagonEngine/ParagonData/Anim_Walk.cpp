48bd3efdb67625ff06a2fa5fad6885b9231d4846
Min-seo-Choi <mschoi0814@gmail.com>
2024-05-16 16:12:37 +0900

    [최민서] BT를 사용해 몬스터 행동 패턴 구현 중

--- ParagonEngine/ParagonData/Anim_Walk.cpp
+++ ParagonEngine/ParagonData/Anim_Walk.cpp
@@ -1,25 +1,25 @@
 #include "Anim_Walk.h"
-
 #include "SkinnedMeshRenderer.h"
+
+//#include "../ParagonUtil/TimeSystem.h"
 #include "../ParagonUtil/Log.h"
+//#include <singleton-cpp/singleton.h>
 
 namespace Pg::Data::BTree::Node
 {
+	//auto& tdelta = singleton<Pg::Util::Time::TimeSystem>();
+	//_deltaTime = &tdelta;
+
 	BT::NodeStatus Anim_Walk::tick()
 	{
 		auto tMeshRenderer = this->GetGameObject()->GetComponent<Pg::Data::SkinnedMeshRenderer>();
 		if (tMeshRenderer != nullptr)
 		{
-			PG_TRACE("Anim_Walk");
-
 			if (tMeshRenderer->GetAnimation() != "PpakMonster_Punch.pganim")
 			{
+				PG_TRACE("Anim_Walk");
 				tMeshRenderer->SetAnimation("PpakMonster_Punch.pganim", true);
 				return BT::NodeStatus::SUCCESS;
-			}
-			else
-			{
-				return BT::NodeStatus::FAILURE;
 			}
 		}
 
