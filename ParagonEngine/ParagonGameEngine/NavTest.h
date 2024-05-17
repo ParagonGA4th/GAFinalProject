2d7997e86e924fac8dc10c2d7ccccdc0258477e9
Jisang Byeon <pannasonic74@gmail.com>
2024-05-17 17:37:07 +0900

    [변지상] 경로 그러짐. 하지만 보이지 않아서 확인 필요함

--- ParagonEngine/ParagonGameEngine/NavTest.h
+++ ParagonEngine/ParagonGameEngine/NavTest.h
@@ -1,6 +1,7 @@
 #pragma once
 #pragma once
 #include "../ParagonData/Component.h"
+#include "../ParagonMath/PgMath.h"
 
 /// <summary>
 /// 구현한 각종 이벤트들을 테스트할 스크립트.
@@ -9,6 +10,7 @@
 namespace Pg::Engine
 {
 	class Navigation;
+	class DebugSystem;
 
 	class NavTest : public Pg::Data::Component
 	{
@@ -19,8 +21,14 @@ namespace Pg::Engine
 		virtual void Start() override;
 		virtual void Update() override;
 
+		void Move(Pg::Math::PGFLOAT3 startPos, Pg::Math::PGFLOAT3 targetPos);
+
 	private:
+
+		std::vector<std::pair<Pg::Math::PGFLOAT3, Pg::Math::PGFLOAT3>> _straightpath;
+
 		Pg::Engine::Navigation* _navSystem = nullptr;
+		Pg::Engine::DebugSystem* _debugSystem = nullptr;
 	};
 }
 
