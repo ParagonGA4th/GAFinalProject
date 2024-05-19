fbd16d63699f4de209e16ee43ea1715cae768324
Min-seo-Choi <mschoi0814@gmail.com>
2024-05-19 21:21:18 +0900

    [최민서] 애니매이션과 애니매이션 사이 딜레이 성공

--- ParagonEngine/ParagonData/Anim_Walk.cpp
+++ ParagonEngine/ParagonData/Anim_Walk.cpp
@@ -1,32 +1,32 @@
 #include "Anim_Walk.h"
 #include "SkinnedMeshRenderer.h"
 
-//#include "../ParagonUtil/TimeSystem.h"
 #include "../ParagonUtil/Log.h"
-//#include <singleton-cpp/singleton.h>
+
+#include <string>
 
 namespace Pg::Data::BTree::Node
 {
-	//auto& tdelta = singleton<Pg::Util::Time::TimeSystem>();
-	//_deltaTime = &tdelta;
-
 	BT::NodeStatus Anim_Walk::tick()
 	{
-		PG_TRACE("Anim_Walk");
 		auto tMeshRenderer = this->GetGameObject()->GetComponent<Pg::Data::SkinnedMeshRenderer>();
 		if (tMeshRenderer != nullptr)
 		{
-			if (tMeshRenderer->GetAnimation() != "PpakMonster_Punch.pganim")
+			config().blackboard->set<std::string>("CURRENTANIM", "Walk");
+			bool isChange = config().blackboard->get<bool>("ISCHANGE");
+
+			if (!isChange && tMeshRenderer->GetAnimation() != "PpakMonster_Punch.pganim")
 			{
 				tMeshRenderer->SetAnimation("PpakMonster_Punch.pganim", true);
-				PG_TRACE("Anim_Walk_SUCCESS");
-				//return BT::NodeStatus::SUCCESS;
+				config().blackboard->set<bool>("ISCHANGE", true);
+				PG_TRACE("Walk_Set");
+				return BT::NodeStatus::SUCCESS;
 			}
-			//else
-			//{
-			//	PG_TRACE("Anim_Walk_FAILURE");
-			//	return BT::NodeStatus::FAILURE;
-			//}
+			else if (isChange && tMeshRenderer->GetAnimation() == "PpakMonster_Punch.pganim")
+			{
+				PG_TRACE("Walk_Load");
+				return BT::NodeStatus::FAILURE;
+			}
 		}
 
 		return BT::NodeStatus::SUCCESS;
