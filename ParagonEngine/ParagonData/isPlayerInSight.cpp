574a959dad1f061f88a2c534ad3d3de36e267f45
Min-seo-Choi <mschoi0814@gmail.com>
2024-05-19 22:01:03 +0900

    [최민서] bt에서 sight 처리 관련

--- ParagonEngine/ParagonData/isPlayerInSight.cpp
+++ ParagonEngine/ParagonData/isPlayerInSight.cpp
@@ -1,12 +1,28 @@
 #include "isPlayerInSight.h"
+#include "../ParagonScript/EnemySight.h"
 #include "../ParagonUtil/Log.h"
-
+#include <string>
 
 namespace Pg::Data::BTree::Node
 {
 	BT::NodeStatus isPlayerInSight::tick()
 	{
-		PG_TRACE("PlayerInSIght");
+		auto children = this->GetGameObject()->_transform.GetChildren();
+		for (auto& child : children)
+		{
+			if (child->_object->GetComponent<Pg::DataScript::EnemySight>() != nullptr)
+			{
+				auto sight = child->_object->GetComponent<Pg::DataScript::EnemySight>();
+				if (sight->_playerDetected)
+				{
+					return BT::NodeStatus::FAILURE;
+				}
+				else
+				{
+					return BT::NodeStatus::SUCCESS;
+				}
+			}
+		}
 		return BT::NodeStatus::SUCCESS;
 	}
 }
