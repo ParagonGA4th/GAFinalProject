aca949a6a9f3cef2fe6368985ef78f269d397629
Min-seo-Choi <mschoi0814@gmail.com>
2024-05-23 14:52:27 +0900

    [최민서] LAYER_BACKGROUND 추가

--- ParagonEngine/ParagonData/LayerMask.h
+++ ParagonEngine/ParagonData/LayerMask.h
@@ -9,5 +9,6 @@ namespace Pg::Data::Enums
 		LAYER_MONSTER = 2,		// 몬스터, 적들의 레이어
 		LAYER_PROJECTILES = 3,	// 날아다니는 투사체들의 Area.
 		LAYER_MAP = 4,			// 맵 기믹(함정, 전투 등)
+		LAYER_BACKGROUND = 5,	// 맵 배경
 	};
 }
