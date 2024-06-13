3d4d6b6bc38663aa1709ec22ffb7406f909075b5
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2024-06-13 17:41:09 +0900

    [고태욱] 애니메이션 문제 해결! 이제, 메인이랑 합치기 전에 Graphics 대폭 정리 + 금요일에는 새로운 맵 로드에 올인.

--- ParagonEngine/ShaderResources/Shaders/System/Pipeline/FirstStage/FirstSkinned_VS.hlsl
+++ ParagonEngine/ShaderResources/Shaders/System/Pipeline/FirstStage/FirstSkinned_VS.hlsl
@@ -21,8 +21,9 @@ VOut1st main(Vin1stPassSkinned_Layout input)
                                mul(mul(gCBuf_Bones[input.vin1st_BlendIndice3], gCBuf_Nodes[input.vin1st_BlendIndice3]), LastWeight);
 	
 	
-    float3 skinnedPosL = mul(float4(input.vin1st_PosL, 1.0f), finalOffsetMatrix);
-	
+    float4 skinnedPosL = mul(float4(input.vin1st_PosL, 1.0f), finalOffsetMatrix);
+    skinnedPosL /= skinnedPosL.w;
+    
 	//NodeIndex를 쓰지 않는다..?? 일단 보류.
 	
 	// TODO : offsetMatrix와 node matrix도 곱한 matrix의 역전치를 곱해야될거같은데 일단 보류
@@ -31,12 +32,13 @@ VOut1st main(Vin1stPassSkinned_Layout input)
     float3 skinnedTangentL = normalize(mul(float4(input.vin1st_TangentL, 1.0f), finalOffsetMatrix));
 	
 	// Position을 Local -> World 이동.
-    output.vout1st_PosW = mul(gCBuf_World, float4(skinnedPosL, 1.0f)).xyz;
+    //output.vout1st_PosW = mul(gCBuf_World, float4(skinnedPosL, 1.0f)).xyz;
+    output.vout1st_PosW = mul(gCBuf_World, skinnedPosL).xyz;
 	
     // 동차좌표계 내 Position 계산.
     // Direct3D->HLSL에서 Row->ColumnMajor로 들어온 것이기에, WorldViewProj 대신함. (연산 곱 순서 바뀜)
 	// WorldViewProj = mul(gCBuf_ViewProjMatrix, gCBuf_World)
-    output.vout1st_PosH = mul(mul(gCBuf_SwitchableViewProj, gCBuf_World), float4(skinnedPosL, 1.0f));
+    output.vout1st_PosH = mul(mul(gCBuf_SwitchableViewProj, gCBuf_World), skinnedPosL);
 	
     output.vout1st_MeshMatID = input.vin1st_MeshMatID;
     output.vout1st_Tex = input.vin1st_Tex;
@@ -50,7 +52,7 @@ VOut1st main(Vin1stPassSkinned_Layout input)
 	
     //무조건 LightPos 기준으로 기록. 일반적으로 그려질 때, 이는 기록될 것이다.
 	//output.vout1st_Color = input.vin1st_Color;
-    float4 tLightPosH = mul(mul(_lightViewProj, gCBuf_World), float4(skinnedPosL, 1.0f));
+    float4 tLightPosH = mul(mul(_lightViewProj, gCBuf_World), skinnedPosL);
     output.vout1st_LightPixelPos = tLightPosH.xyz / tLightPosH.w;
 	
     return output;
