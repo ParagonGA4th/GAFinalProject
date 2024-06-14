489bca7d8ece57569787d399ff17b0a409a1a6fb
Min-seo-Choi <mschoi0814@gmail.com>
2024-06-14 13:53:21 +0900

    [최민서] 그루터기 애니매이션

--- ParagonEngine/ParagonData/isSkillAttack.cpp
+++ ParagonEngine/ParagonData/isSkillAttack.cpp
@@ -3,21 +3,29 @@
 
 namespace Pg::Data::BTree::Node
 {
+	void isSkillAttack::InitCustom()
+	{
+		config().blackboard->set<bool>("ISSKILLANIMEND", false);
+	}
+
 	BT::NodeStatus isSkillAttack::tick()
 	{
+		bool isAnimEnd = config().blackboard->get<bool>("ISSKILLANIMEND");
+
 		auto monHelper = this->GetGameObject()->GetComponent<Pg::Data::MonsterHelper>();
 		if (monHelper != nullptr)
 		{
 			if (monHelper->_isPlayerinHitSpace &&
 				(monHelper->_trentState == Pg::Data::TrentState::IDLE || monHelper->_trentState == Pg::Data::TrentState::BASICATTACK_COOLDOWN))
 			{
+				if(isAnimEnd) config().blackboard->set<bool>("ISSKILLANIMEND", false);
 				monHelper->_trentState = Pg::Data::TrentState::SKILL_ATTACK;
+				
 				return BT::NodeStatus::SUCCESS;
 			}
-			else if(monHelper->_trentState == Pg::Data::TrentState::SKILL_ATTACK && monHelper->_isAnimationEnd)
+			else if (monHelper->_trentState == Pg::Data::TrentState::SKILL_ATTACK && !isAnimEnd)
 			{
-				monHelper->_isAnimationEnd = false;
-				return BT::NodeStatus::FAILURE;
+				return BT::NodeStatus::SUCCESS;
 			}
 		}
 
