5c345ee7997d8c7795f0d71f5e3f8f0d8799894e
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2024-06-15 22:15:35 +0900

    [고태욱] Game Flow 관련 작업 추가 작업. Handler 기반 구조 설정.

--- ParagonEngine/ParagonScript/TotalGameManager.h
+++ ParagonEngine/ParagonScript/TotalGameManager.h
@@ -3,8 +3,10 @@
 #include "GameState.h"
 
 #include "../ParagonData/Scene.h"
+#include "../ParagonData/GameObject.h"
 
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 /// <summary>
@@ -16,6 +18,11 @@ namespace Pg::API { class PgScene; }
 
 namespace Pg::DataScript
 {
+	class IAreaHandler;
+	class IEnemyHandler;
+	class IGUIHandler;
+	class PlayerBattleBehavior;
+
 	class TotalGameManager : public ScriptInterface<TotalGameManager>
 	{
 		DEFINE_PARAGON_SCRIPT_SINGLETON(TotalGameManager);
@@ -30,6 +37,22 @@ namespace Pg::DataScript
 		virtual void OnSceneChange_Global(Pg::Data::Scene* changedScene) override;
 	private:
 		Pg::API::PgScene* _pgScene{ nullptr };
+
+
+	public:
+		//개별적인 Struct 관리. Flow 관련된 구조체들을 묶어 놓은 것이다.
+		//모두 다 개별 3D Scene에 개별적으로 존재해야 하는 애들이다.
+		struct HandlerBundle
+		{
+			IAreaHandler* _areaHandler = nullptr;
+			IEnemyHandler* _enemyHandler = nullptr;
+			IGUIHandler* _guiHandler = nullptr;
+			PlayerBattleBehavior* _playerBehavior = nullptr;
+		};
+
+	private:
+		//Flow 관리를 위해 보관하는 정보.
+		std::unordered_map<Pg::Data::Scene*, HandlerBundle> _scene3dHandlerBundleMap;
 	};
 }
 
