919d09eac1eabb6c7bbaace6a11ac70f93760cda
Jisang Byeon <pannasonic74@gmail.com>
2024-06-17 19:29:16 +0900

    [변지상] 보스의 행동 로직 구현 중...

---
+++ ParagonEngine/ParagonScript/BossInfo.cpp
@@ -0,0 +1,21 @@
+#include "BossInfo.h"
+
+namespace Pg::DataScript
+{
+	BossInfo::BossInfo(float fullHealth, float attackPower) : BaseMonsterInfo(fullHealth, attackPower),
+		_moveSpeed(0.2f), _dashSpeed(1.4f), _attackRange(4.f),
+		_sightRange(15.f), _dashRange(14.f), _dashDuration(1.f)
+	{
+
+	}
+
+	void BossInfo::HandleEvents(const IEvent& e, UsedVariant usedVar1, UsedVariant usedVar2)
+	{
+
+	}
+
+	void BossInfo::ResetAll()
+	{
+
+	}
+}
