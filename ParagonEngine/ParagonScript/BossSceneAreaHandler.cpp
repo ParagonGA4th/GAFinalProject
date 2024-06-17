91d389e81d0838b5332f94afbc87d475f66fba5d
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2024-06-17 19:18:53 +0900

    [고태욱] 필요한 Script들 껍데기 추가 + 필요 요소 점검 중.

---
+++ ParagonEngine/ParagonScript/BossSceneAreaHandler.cpp
@@ -0,0 +1,30 @@
+#include "BossSceneAreaHandler.h"
+
+namespace Pg::DataScript
+{
+	BossSceneAreaHandler::BossSceneAreaHandler(Pg::Data::GameObject* obj) : ScriptInterface(obj)
+	{
+
+	}
+
+	void BossSceneAreaHandler::GrabManagedObjects()
+	{
+
+	}
+
+	void BossSceneAreaHandler::Start()
+	{
+
+	}
+
+	void BossSceneAreaHandler::Update()
+	{
+
+	}
+
+	void BossSceneAreaHandler::ResetToInitialState()
+	{
+
+	}
+
+}
---
+++ ParagonEngine/ParagonScript/Stage1EnemyHandler.cpp
@@ -0,0 +1,31 @@
+#include "Stage1EnemyHandler.h"
+
+namespace Pg::DataScript
+{
+
+	Stage1EnemyHandler::Stage1EnemyHandler(Pg::Data::GameObject* obj) : ScriptInterface(obj)
+	{
+
+	}
+
+	void Stage1EnemyHandler::GrabManagedObjects()
+	{
+
+	}
+
+	void Stage1EnemyHandler::Start()
+	{
+
+	}
+
+	void Stage1EnemyHandler::Update()
+	{
+
+	}
+
+	void Stage1EnemyHandler::ResetToInitialState()
+	{
+
+	}
+
+}
