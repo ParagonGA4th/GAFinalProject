c8c0a6a4205c2f7dd28b7443126ef702e5eec49a
Jisang Byeon <pannasonic74@gmail.com>
2024-06-17 19:29:16 +0900

    [변지상] 보스의 행동 로직 구현 중...

--- ParagonEngine/ParagonScript/BossBehaviour.h
+++ ParagonEngine/ParagonScript/BossBehaviour.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "ScriptInterface.h"
-#include "EnemyInfo.h"
+#include "BossInfo.h"
 #include "BaseMonster.h"
 
 namespace Pg::Data
@@ -37,17 +37,27 @@ namespace Pg::DataScript
 		virtual void Start() override;
 		virtual void Update() override;
 
-		//공격에 맞았을 때
-		virtual void OnCollisionEnter(Pg::Data::PhysicsCollision** _colArr, unsigned int count);
-
 		//플레이어를 쫓는 함수
 		void Chase();
 
 		//타겟의 위치를 향해 바라봄.
 		void RotateToPlayer(Pg::Math::PGFLOAT3& targetPos);
 
+		//특정 방향을 향해 돌진.
+		//미니골렘과는 달리 플레이어를 향하는 것이 아니라 자신이 보는 방향을 향해 돌진한다.
+		void Dash();
+
 		//플레이어를 공격.
 		void Attack();
+
+		//회피하는 로직.
+		void Avoid();
+
+		//보스의 무력화 상태
+		void neutralize();
+
+		//피격 시 행동.
+		void Hit();
 
 		//피격 시 죽음.
 		void Dead();
@@ -65,10 +75,13 @@ namespace Pg::DataScript
 
 	public:
 		//골렘보스의 상태와 수치에 대한 정보.
-		BossGolemInfo _bossGolInfo;
-		BossGolemStatus _bossGolStat;
+		BossInfo* _bossInfo;
 
 		bool _isRotateFinish;
+
+		//대쉬 관련 변수.
+		bool _isDash;			//돌진 여부
+		bool _hasDashed;		//돌진했는지 여부
 	};
 }
 
