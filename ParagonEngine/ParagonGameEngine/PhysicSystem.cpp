e1f0e42779ab6b9eadb57ae17d9ec101b1822dd9
Jisang Byeon <pannasonic74@gmail.com>
2024-06-19 13:50:58 +0900

    [변지상] 몬스터끼리 충돌 못하게 막음 + 미니골렘 사운드 세부조정

--- ParagonEngine/ParagonGameEngine/PhysicSystem.cpp
+++ ParagonEngine/ParagonGameEngine/PhysicSystem.cpp
@@ -127,7 +127,7 @@ namespace Pg::Engine::Physic
 			//전체 테스팅, 프로그래머가 2차원 배열 전체 내용 맞게 커밋해놓자. (-> ex. (0,1) 이 0bit이라면, (1,0)도 똑같이 0bit으로 해놓는 것!
 			Pg::Engine::PgLayer::SetCollisionData(LAYER_DEFAULT, { LAYER_DEFAULT, LAYER_PLAYER, LAYER_MONSTER, LAYER_PROJECTILES });
 			Pg::Engine::PgLayer::SetCollisionData(LAYER_PLAYER, { LAYER_DEFAULT, LAYER_MONSTER, LAYER_MAP });
-			Pg::Engine::PgLayer::SetCollisionData(LAYER_MONSTER, { LAYER_DEFAULT, LAYER_PLAYER,  LAYER_MONSTER, LAYER_PROJECTILES, LAYER_MAP });
+			Pg::Engine::PgLayer::SetCollisionData(LAYER_MONSTER, { LAYER_DEFAULT, LAYER_PLAYER, LAYER_PROJECTILES, LAYER_MAP });
 			Pg::Engine::PgLayer::SetCollisionData(LAYER_PROJECTILES, { LAYER_DEFAULT,   LAYER_MONSTER, }); //Projectile 기준으로 자기 자신과 플레이어, 이렇게 충돌 못하게. 
 			Pg::Engine::PgLayer::SetCollisionData(LAYER_MAP, { LAYER_PLAYER,   LAYER_MONSTER, }); //무조건 Layer 5여야 한다. 다른 대안은 존재 X.
 		}
