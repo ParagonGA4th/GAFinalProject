15630073a156207295b4e6c79e59006c759f9ed8
Min-seo-Choi <mschoi0814@gmail.com>
2024-06-22 20:39:31 +0900

    [최민서] get/set uuid 삭제 및 serializer 시 dontdestroy노드 true 입력 되는 것 오류 수정. 플레이어 공격 애니매이션 추가

--- ParagonEngine/ParagonScript/PlayerHandler.h
+++ ParagonEngine/ParagonScript/PlayerHandler.h
@@ -30,6 +30,7 @@ namespace Pg::Data
 {
 	class DynamicCollider;
 	class AudioSource;
+	class SkinnedMeshRenderer;
 }
 
 namespace Pg::DataScript
@@ -60,12 +61,17 @@ namespace Pg::DataScript
 		//IScriptResettable. 다시 자기 자신을 리셋하는 함수.
 		virtual void ResetAll() override;
 
+		//Animation이 끝났을 때 호출 되는 함수
+		virtual void OnAnimationEnd() override;
+
 	private:
 		void FindAllArrowsInMap();
 		void ArrowShootingLogic();
 		void CalculateMonsterDamages();
 		void CalculateMonsterHit();
 
+	private:
+		void PlayAdequateAnimation();	// 플레이어 행동에 따른 맞는 애니매이션 출력
 
 	public:
 		//플레이어에게 들어오는 개별적인 로직은 따로 분리됨.
@@ -75,6 +81,7 @@ namespace Pg::DataScript
 
 		void AddMonsterHitList(BaseMonsterInfo* monster, float healthChangeLvl);
 		void AddMonsterOnHitList(BaseMonsterInfo* monster);
+
 
 	public:
 		//체력 관련. -> 지금은 맵 기믹때문에 이렇게 해놨지만, 나중에는 별도로 이동해야.
@@ -101,12 +108,20 @@ namespace Pg::DataScript
 		Pg::API::Time::PgTime* _pgTime;
 		std::vector<ArrowLogic*> _arrowVec;
 		Pg::Data::DynamicCollider* _selfCol{ nullptr };
+		Pg::Data::SkinnedMeshRenderer* _meshRenderer{ nullptr };
 		PlayerMovement* _playerMovement;
 
 	private:
 		//사운드 관련 변수
 		Pg::Data::GameObject* _commonAttackSound;
 		Pg::Data::AudioSource* _commonAttackAudio;
+
+
+	private:
+		// 플레이어 애니매이션 관련 변수
+		std::string _prevAnimationInput;
+		bool _isHit;
+		int _hitCount = 0;
 	};
 }
 
