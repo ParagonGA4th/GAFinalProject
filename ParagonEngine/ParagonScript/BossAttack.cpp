3b8bce3510d6e6094f675f76e31a98eeaec94a54
Jisang Byeon <pannasonic74@gmail.com>
2024-06-25 20:25:01 +0900

    [변지상] BossAttack 추가 후 테스트 완료

---
+++ ParagonEngine/ParagonScript/BossAttack.cpp
@@ -0,0 +1,45 @@
+#include "BossAttack.h"
+#include "CombatSystem.h"
+#include "../ParagonData/Collider.h"
+#include "../ParagonUtil/Log.h"
+
+namespace Pg::DataScript
+{
+
+	BossAttack::BossAttack(Pg::Data::GameObject* obj) :
+		ScriptInterface(obj)
+	{
+
+	}
+
+	void BossAttack::Awake()
+	{
+
+	}
+
+	void BossAttack::Start()
+	{
+		_combatSystem = CombatSystem::GetInstance(nullptr);
+	}
+
+	void BossAttack::Update()
+	{
+
+	}
+
+	void BossAttack::OnTriggerEnter(Pg::Data::Collider** _colArr, unsigned int count)
+	{
+		
+		for (int i = 0; i < count; i++)
+		{
+			Pg::Data::Collider* col = _colArr[i];
+
+			//플레이어한테 데미지를 주어라
+			if (col->_object->GetTag() == "TAG_Player")
+			{
+				PG_TRACE("Player Hit!");
+				_combatSystem->ChangePlayerHealth(-2.f);
+			}
+		}
+	}
+}
