0e628dd2dc10423c161255c46e53488d7e90a14d
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2024-07-02 15:38:24 +0900

    [고태욱] 2D : Scrolling Background + Animated Textures 정상 작동 확인

--- ParagonEngine/ParagonScript/AddEffectorTest.cpp
+++ ParagonEngine/ParagonScript/AddEffectorTest.cpp
@@ -1,10 +1,15 @@
 #include "AddEffectorTest.h"
+#include "../ParagonAPI/PgGraphics.h"
+#include "../ParagonAPI/PgInput.h"
+
+#include <singleton-cpp/singleton.h>
+#include <windows.h>
 
 namespace Pg::DataScript
 {
 	AddEffectorTest::AddEffectorTest(Pg::Data::GameObject* obj) : ScriptInterface(obj)
 	{
-
+		
 	}
 
 	AddEffectorTest::~AddEffectorTest()
@@ -14,17 +19,32 @@ namespace Pg::DataScript
 
 	void AddEffectorTest::Awake()
 	{
-
+		_pgGraphics = &singleton<Pg::API::Graphics::PgGraphics>();
+		_pgInput = &singleton<Pg::API::Input::PgInput>();
 	}
 
 	void AddEffectorTest::Start()
 	{
-
+		
 	}
 
 	void AddEffectorTest::Update()
 	{
-
+		if (_pgInput->GetKeyDown(API::Input::KeyNum7))
+		{
+			tObj = new Pg::Data::VisualEffectRenderObject();
+			tObj->_position = { 500,500,0 };
+			//_pgGraphics->RegisterEffectObject("Effect_FireArrow", tObj);
+			_pgGraphics->RegisterEffectObject("Effect_Credits", tObj);
+		}
+		else if (_pgInput->GetKeyDown(API::Input::KeyNum8))
+		{
+			if (tObj != nullptr)
+			{
+				_pgGraphics->RemoveEffectObject(tObj);
+				delete tObj;
+			}
+		}
 	}
 
 }
