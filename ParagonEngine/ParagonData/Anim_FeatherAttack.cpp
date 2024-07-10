27e6aaee8966232ad1b318e37838be611b7ad20d
Min-seo-Choi <mschoi0814@gmail.com>
2024-07-10 23:46:31 +0900

    [최민서] 보스 애니매이션 및 서브 보스 애니매이션

--- ParagonEngine/ParagonData/Anim_FeatherAttack.cpp
+++ ParagonEngine/ParagonData/Anim_FeatherAttack.cpp
@@ -9,25 +9,44 @@ namespace Pg::Data::BTree::Node
 		auto monHelper = this->GetGameObject()->GetComponent<Pg::Data::MonsterHelper>();
 		if (monHelper != nullptr)
 		{
-			if (monHelper->_bossFlag._bossState != Pg::Data::BossState::SKILL_FEATHER_ATTACK)
+			if (monHelper->_bossFlag._bossStateListByEnum[monHelper->_bossFlag._bossState].find("SFeatherAttack") == std::string::npos)
 				return BT::NodeStatus::FAILURE;
 
 			if (monHelper->_isAnimationEnd)
 			{
+				if (monHelper->_bossFlag._bossState == Pg::Data::BossState::SKILL_FEATHER_ATTACK_PREPARE)
+					monHelper->_bossFlag._bossState = Pg::Data::BossState::SKILL_FEATHER_ATTACK;
+
+				if (monHelper->_bossFlag._bossState == Pg::Data::BossState::SKILL_FEATHER_ATTACK)
+				{
+					_isInit = false;
+					monHelper->_bossFlag._bossState = Pg::Data::BossState::SKILL_FEATHER_ATTACK_END;
+				}
+
+				if (monHelper->_bossFlag._bossState == Pg::Data::BossState::SKILL_FEATHER_ATTACK_END)
+					monHelper->_bossFlag._bossState = Pg::Data::BossState::IDLE;	
+
 				monHelper->_isAnimationEnd = false;
-				monHelper->_bossFlag._bossState = Pg::Data::BossState::IDLE;
+
+				return BT::NodeStatus::FAILURE;
 			}
-		}
 
-		auto tMeshRenderer = this->GetGameObject()->GetComponent<Pg::Data::SkinnedMeshRenderer>();
-		if (tMeshRenderer != nullptr)
-		{
-			std::string animId = tMeshRenderer->GetAnimation().substr(0, tMeshRenderer->GetAnimation().find("_"));
-			animId.append("_00008.pganim");
 
-			if (tMeshRenderer->GetAnimation() != animId)
+			auto tMeshRenderer = this->GetGameObject()->GetComponent<Pg::Data::SkinnedMeshRenderer>();
+			if (tMeshRenderer != nullptr)
 			{
-				tMeshRenderer->SetAnimation(animId, false);
+				bool _loop = false;
+				std::string animId = tMeshRenderer->GetAnimation().substr(0, tMeshRenderer->GetAnimation().find("_"));
+				if (!_isInit) animId.append("_00008.pganim");
+				else animId.append("_00017.pganim"); _loop = true;
+
+				if (monHelper->_bossFlag._bossState == Pg::Data::BossState::SKILL_FEATHER_ATTACK_END)
+					animId.append("_00018.pganim");
+
+				if (tMeshRenderer->GetAnimation() != animId)
+				{
+					tMeshRenderer->SetAnimation(animId, _loop);
+				}
 			}
 		}
 
