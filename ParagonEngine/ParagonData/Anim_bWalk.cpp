24f08df76908b65fa98206b3ce79d20274b7d90b
Min-seo-Choi <mschoi0814@gmail.com>
2024-07-12 14:19:21 +0900

    [최민서] 보스 Chase

--- ParagonEngine/ParagonData/Anim_bWalk.cpp
+++ ParagonEngine/ParagonData/Anim_bWalk.cpp
@@ -1,19 +1,25 @@
 #include "Anim_bWalk.h"
 #include "SkinnedMeshRenderer.h"
+#include "MonsterHelper.h"
 
 namespace Pg::Data::BTree::Node
 {
 	BT::NodeStatus Anim_bWalk::tick()
 	{
-		auto tMeshRenderer = this->GetGameObject()->GetComponent<Pg::Data::SkinnedMeshRenderer>();
-		if (tMeshRenderer != nullptr)
+		auto monHelper = this->GetGameObject()->GetComponent<Pg::Data::MonsterHelper>();
+		if (monHelper != nullptr)
 		{
-			std::string animId = tMeshRenderer->GetAnimation().substr(0, tMeshRenderer->GetAnimation().find("_"));
-			animId.append("_00002.pganim");
-
-			if (tMeshRenderer->GetAnimation() != animId)
+			auto tMeshRenderer = this->GetGameObject()->GetComponent<Pg::Data::SkinnedMeshRenderer>();
+			if (tMeshRenderer != nullptr)
 			{
-				tMeshRenderer->SetAnimation(animId, true);
+				std::string animId = tMeshRenderer->GetAnimation().substr(0, tMeshRenderer->GetAnimation().find("_"));
+				animId.append("_00002.pganim");
+
+				if (tMeshRenderer->GetAnimation() != animId)
+				{
+					monHelper->_bossFlag._bossState = Pg::Data::BossState::CHASE;
+					tMeshRenderer->SetAnimation(animId, true);
+				}
 			}
 		}
 
