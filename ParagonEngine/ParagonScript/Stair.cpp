17e4a89e6d3c2c4abbe966b31e3e08b1467da61b
Min-seo-Choi <mschoi0814@gmail.com>
2024-07-14 19:44:08 +0900

    [최민서] 박스 추가시 씬 로딩 안되는 오류 수정 및 새로운 기믹 클래스 추가

---
+++ ParagonEngine/ParagonScript/Stair.cpp
@@ -0,0 +1,21 @@
+#include "Stair.h"
+
+namespace Pg::DataScript
+{
+	Stair::Stair(Pg::Data::GameObject* obj)
+		:ScriptInterface(obj)
+	{
+	}
+	
+	void Stair::BeforePhysicsAwake()
+	{
+	}
+
+	void Stair::Awake()
+	{
+	}
+	
+	void Stair::Update()
+	{
+	}
+}
