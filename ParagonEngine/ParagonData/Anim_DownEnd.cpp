9cc5f4e2cd7624b382a59c6ee7174d7d2df99491
Min-seo-Choi <mschoi0814@gmail.com>
2024-07-22 22:00:10 +0900

    [최민서] 보스 애니매이션 수정 중

--- ParagonEngine/ParagonData/Anim_DownEnd.cpp
+++ ParagonEngine/ParagonData/Anim_DownEnd.cpp
@@ -11,8 +11,6 @@ namespace Pg::Data::BTree::Node
 		{
 			if (monHelper->_isAnimationEnd)
 			{
-				monHelper->_bossFlag._isDownEnd = false;
-
 				if (monHelper->_bossFlag._bossPase == Pg::Data::BossPase::PASE_1)
 				{
 					monHelper->_bossFlag._bossPase == Pg::Data::BossPase::PASE_2;
@@ -21,9 +19,6 @@ namespace Pg::Data::BTree::Node
 				{
 					monHelper->_bossFlag._bossPase == Pg::Data::BossPase::PASE_3;
 				}
-
-				monHelper->_isAnimationEnd = false;
-
 				return BT::NodeStatus::FAILURE;
 			}
 
