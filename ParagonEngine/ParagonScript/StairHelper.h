5a3c71e6c5ef540ce74b3ff1c46250288e02a3ea
Taewook Koh <98639611+TaewookWorks@users.noreply.github.com>
2024-07-26 17:02:48 +0900

    [고태욱]  오류 수정

--- ParagonEngine/ParagonScript/StairHelper.h
+++ ParagonEngine/ParagonScript/StairHelper.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "ScriptInterface.h"
 #include "../ParagonData/GameObject.h"
+#include "IObserver.h"
 #include "../ParagonData/VisualEffectRenderObject.h"
 
 namespace Pg::API
@@ -16,20 +17,22 @@ namespace Pg::Data
 
 namespace Pg::DataScript
 {
-	class StairHelper : public ScriptInterface<StairHelper>
+	class StairHelper : public ScriptInterface<StairHelper>, public IObserver
 	{
 		DEFINE_PARAGON_SCRIPT(StairHelper);
 
 	public:
 		StairHelper(Pg::Data::GameObject* obj);
 		~StairHelper();
+		virtual void GrabManagedObjects() override;
 		virtual void Awake() override;
 
 		virtual void OnTriggerEnter(Pg::Data::Collider** _colArr, unsigned int count) override;
 		virtual void OnTriggerExit(Pg::Data::Collider** _colArr, unsigned int count) override;
 
 		void EffectHelper(bool isActive);
-
+		virtual void HandleEvents(const IEvent& e, UsedVariant usedVar1, UsedVariant usedVar2) override;
+		virtual void CleanOnSceneChange();
 
 		bool _onTriggerStay;
 
